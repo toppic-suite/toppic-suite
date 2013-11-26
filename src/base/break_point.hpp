804c9fdfdd9e60a4f26a75a7d51ae95a9dd930de
likxun <xunlikun@qq.com>
2013-11-26 12:10:32 -0500

    reason:modify rename file name:break_point by:xunlikun

---
+++ src/base/break_point.hpp
@@ -0,0 +1,31 @@
+/*
+ * BreakPoint.hpp
+ *
+ *  Created on: Nov 26, 2013
+ *      Author: xunlikun
+ */
+
+#ifndef PROT_BREAK_POINT_H_
+#define PROT_BREAK_POINT_H_
+
+#include "ion_type.hpp"
+
+namespace prot {
+
+class BreakPoint {
+public:
+	BreakPoint(double prm,double srm);
+	double getPrm(){return prm_;}
+	double getSrm(){return srm_;}
+	double getNTermMass(IonTypePtr ion_type){return prm_ + ion_type->getShift();}
+	double getCTermMass(IonTypePtr ion_type){return srm_ + ion_type->getShift();}
+private:
+	double prm_;
+	double srm_;
+};
+
+typedef std::shared_ptr<BreakPoint> BreakPointPtr;
+
+} /* namespace prot */
+
+#endif /* BREAKPOINT_HPP_ */
