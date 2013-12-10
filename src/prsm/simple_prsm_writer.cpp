58dade9eca8dadaaa06c577740657c04185cc95b
xunlikun <xunlikun@xlk-vm>
2013-12-10 16:51:59 -0500

    add prsm writer

---
+++ src/prsm/simple_prsm_writer.cpp
@@ -0,0 +1,12 @@
+/*
+ * simple_prsm_writer.cpp
+ *
+ *  Created on: Dec 9, 2013
+ *      Author: xunlikun
+ */
+
+#include <prsm/simple_prsm_writer.hpp>
+
+namespace prot {
+
+} /* namespace prot */
---
+++ src/prsm/simple_prsm_writer.hpp
@@ -0,0 +1,20 @@
+/*
+ * simple_prsm_writer.hpp
+ *
+ *  Created on: Dec 9, 2013
+ *      Author: xunlikun
+ */
+
+#ifndef PROT_SIMPLE_PRSM_WRITER_HPP_
+#define PROT_SIMPLE_PRSM_WRITER_HPP_
+
+namespace prot {
+
+class SimplePrSMWriter {
+public:
+	SimplePrSMWriter(const char *spectrum_file);
+};
+
+} /* namespace prot */
+
+#endif /* SIMPLE_PRSM_WRITER_HPP_ */
