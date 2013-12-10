35f48b8cf82cdce7e1ddb3c658fbb10554a3d6a4
xunlikun <xunlikun@xlk-vm>
2013-12-10 16:52:42 -0500

    add spectrum_set

---
+++ src/spec/spectrum_set.cpp
@@ -0,0 +1,41 @@
+/*
+ * spectrum_set.cpp
+ *
+ *  Created on: Dec 9, 2013
+ *      Author: xunlikun
+ */
+
+#include <spec/spectrum_set.hpp>
+#include "base/base_data.hpp"
+
+namespace prot {
+
+SpectrumSet::SpectrumSet(DeconvMsPtr sp,double delta,SpParaPtr sp_para){
+	//todo:xunlikun@config file name;
+	BaseDataPtr base_data = BaseDataPtr(new BaseData(""));
+	deconv_sp_ = sp;
+	delta_=delta;
+	prm_ms_two_ = prot::getMsTwo(sp,delta,sp_para);
+	extend_ms_three_ = prot::getMsThree(sp,delta,sp_para);
+	prm_ms_six_=prot::getSpSix(sp,delta,sp_para);
+	prm_ms_shift_six_=prot::getShiftSpSix(sp,delta,-prot::getProtModPtrByName(base_data->getProtModPtrVec(),"ACETYLATION")->getPepShift(),sp_para);
+}
+SpectrumSetPtr getSpectrumSet(DeconvMsPtr spectrum,double delta,SpParaPtr sp_para){
+	if(spectrum->size() < sp_para->getMinPeakNum() || spectrum->getHeaderPtr()->getPrecMonoMass() < sp_para->getMinMass()){
+		//logger
+		return nullptr;
+	}
+	if(spectrum->getHeaderPtr()->getActivationPtr() == nullptr){
+		if(sp_para->getActivation()!=nullptr){
+			spectrum->getHeaderPtr()->setActivationPtr(sp_para->getActivation());
+		}
+		else{
+			//logger
+			return nullptr;
+		}
+	}
+
+	return SpectrumSetPtr(new SpectrumSet(spectrum,delta,sp_para));
+}
+
+} /* namespace prot */
---
+++ src/spec/spectrum_set.hpp
@@ -0,0 +1,44 @@
+/*
+ * spectrum_set.hpp
+ *
+ *  Created on: Dec 9, 2013
+ *      Author: xunlikun
+ */
+
+#ifndef PROT_SPECTRUM_SET_HPP_
+#define PROT_SPECTRUM_SET_HPP_
+
+#include <memory>
+#include <vector>
+
+#include "spec/deconv_ms.hpp"
+#include "spec/sp_para.hpp"
+#include "spec/extend_peak.hpp"
+#include "spec/prm_peak.hpp"
+
+namespace prot {
+class SpectrumSet {
+public:
+	SpectrumSet(DeconvMsPtr sp,double delta,SpParaPtr sp_para);
+	DeconvMsPtr getDeconvMs(){return deconv_sp_;}
+	ExtendPeakMs getSpThree(){return extend_ms_three_;}
+	PrmPeakMS getSpTwo(){return prm_ms_two_;}
+	PrmPeakMS getSpSix(){return prm_ms_six_;}
+	PrmPeakMS getSpShiftSix(){return prm_ms_shift_six_;}
+	double getDelta(){return delta_;}
+private:
+	DeconvMsPtr deconv_sp_;
+	double delta_;
+	PrmPeakMS prm_ms_two_;
+	ExtendPeakMs extend_ms_three_;
+	PrmPeakMS prm_ms_six_;
+	PrmPeakMS prm_ms_shift_six_;
+};
+
+typedef std::shared_ptr<SpectrumSet> SpectrumSetPtr;
+
+SpectrumSetPtr getSpectrumSet(DeconvMsPtr spectrum,double delta,SpParaPtr sp_para);
+
+} /* namespace prot */
+
+#endif /* SPECTRUM_SET_HPP_ */
