ac824a80702fa557a4bacfae8e5ff2ef22eb1175
xunlikun <xunlikun@xlk-vm>
2013-12-27 11:43:32 -0500

    SemAlignType

---
+++ src/prsm/sem_align_type.cpp
@@ -0,0 +1,9 @@
+/*
+ * sem_align_type.cpp
+ *
+ *  Created on: Dec 27, 2013
+ *      Author: xunlikun
+ */
+
+#include <prsm/sem_align_type.hpp>
+
---
+++ src/prsm/sem_align_type.hpp
@@ -0,0 +1,14 @@
+/*
+ * sem_align_type.hpp
+ *
+ *  Created on: Dec 27, 2013
+ *      Author: xunlikun
+ */
+
+#ifndef SEM_ALIGN_TYPE_HPP_
+#define SEM_ALIGN_TYPE_HPP_
+
+class SemAlignType {
+};
+
+#endif /* SEM_ALIGN_TYPE_HPP_ */
