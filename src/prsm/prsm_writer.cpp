2e932c7f2258b780d49d9abecc503af57ba399ff
xunlikun <xunlikun@xlk-vm>
2013-12-30 09:47:36 -0500

    prsm writer

---
+++ src/prsm/prsm_writer.cpp
@@ -0,0 +1,12 @@
+/*
+ * prsm_writer.cpp
+ *
+ *  Created on: Dec 30, 2013
+ *      Author: xunlikun
+ */
+
+#include <prsm/prsm_writer.hpp>
+
+namespace prot {
+
+} /* namespace prot */
---
+++ src/prsm/prsm_writer.hpp
@@ -0,0 +1,25 @@
+/*
+ * prsm_writer.hpp
+ *
+ *  Created on: Dec 30, 2013
+ *      Author: xunlikun
+ */
+
+#ifndef PRSM_WRITER_HPP_
+#define PRSM_WRITER_HPP_
+
+namespace prot {
+
+class PrSMWriter {
+public:
+	int write(const char *prm_file_name);
+	void addSimplePrSM(PrSMPtr matches);
+	void addSimplePrSM(PrSMPtrVec matches);
+
+private:
+	PrSMPtrVec prsms_;
+};
+
+} /* namespace prot */
+
+#endif /* PRSM_WRITER_HPP_ */
