a9fdefc7beb533af4bf0d46fe269b4356dd546ce
xunlikun <xunlikun@xlk-vm>
2014-01-13 15:03:02 -0500

    add include

--- src/ptmsearch/ptm_slow_filter.cpp
+++ src/ptmsearch/ptm_slow_filter.cpp
@@ -14,4 +14,14 @@ PtmSlowFilter::PtmSlowFilter(SpectrumSetPtr spectrum_set,SimplePrSMPtrVec fast_M
 		slow_matches_.push_back(PtmSlowMatchPtr(new PtmSlowMatch(seq,spectrum_set,comp_shift,mng)));
 	}
 }
+PtmSlowMatchPtrVec PtmSlowFilter::getBestMatch(int nshift,int type){
+	PtmSlowMatchPtrVec matches;
+	for(int i=0;i<slow_matches_.size();i++){
+		slow_matches_[i]->setShift(nshift);
+		slow_matches_[i]->setType(type);
+		matches.push_back(slow_matches_[i]);
+	}
+
+	std::sort(matches.begin(),matches.end(),prot::psm_up);
+}
 } /* namespace prot */
