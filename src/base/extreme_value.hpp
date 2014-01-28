252920730eccfd26a6ceaa6fbd214a00d9f126c3
xunlikun <xunlikun@xlk-vm>
2014-01-28 10:03:28 -0500

    replace tab with 4 blanks

--- src/base/extreme_value.hpp
+++ src/base/extreme_value.hpp
@@ -13,25 +13,25 @@ class ExtremeValue {
 
   ExtremeValue (xercesc::DOMElement* element);
 
-	double getPValue() {return p_value_;}
-	
-	double getEValue() {return e_value_;}
+    double getPValue() {return p_value_;}
+    
+    double getEValue() {return e_value_;}
 
-	double getOneProtProb() {return one_match_prob_;}
+    double getOneProtProb() {return one_match_prob_;}
 
-	double getTestNum() {return test_num_;}
+    double getTestNum() {return test_num_;}
 
-	double getAdjustFactor() { return adjust_factor_;}
+    double getAdjustFactor() { return adjust_factor_;}
 
   void appendXml(XmlDOMDocument* xml_doc, xercesc::DOMElement* parent);
 
  private:
-	/* oneMatchProb considers expected PTMs, but not unexpected PTMs */
-	double one_match_prob_;
-	double test_num_;
-	double adjust_factor_;
-	double p_value_;
-	double e_value_;
+    /* oneMatchProb considers expected PTMs, but not unexpected PTMs */
+    double one_match_prob_;
+    double test_num_;
+    double adjust_factor_;
+    double p_value_;
+    double e_value_;
 
   void init();
 };
