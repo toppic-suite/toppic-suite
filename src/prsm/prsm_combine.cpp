93586c074c94827e10e195abf75215402e625688
xunlikun <xunlikun@xlk-vm>
2014-02-20 14:05:32 -0500

    add prsm combine

---
+++ src/prsm/prsm_combine.cpp
@@ -0,0 +1,41 @@
+/*
+ * prsm_combine.cpp
+ *
+ *  Created on: Feb 18, 2014
+ *      Author: xunlikun
+ */
+
+#include "prsm/prsm_combine.hpp"
+
+namespace prot {
+
+PrSMCombine::PrSMCombine(std::string db_file,
+                         std::string spec_file,
+                         std::vector<std::string> &in_file_exts,
+                         std::string out_file) {
+  input_file_exts_ = in_file_exts;
+  output_files_ = out_file;
+  spec_file_ = spec_file;
+  db_file_ = db_file;
+}
+
+PrSMCombine::~PrSMCombine() {
+}
+
+void PrSMCombine::process() {
+  ProteoformPtrVec proteoforms = prot::readFastaToProteoform(db_file_,ResidueFactory::getBaseResiduePtrVec());
+  PrSMPtrVec prsms;
+  for(unsigned int i=0;i<input_file_exts_.size();i++){
+    PrSMPtrVec temps = prot::readPrsm(basename(spec_file_)+"."+input_file_exts_[i],proteoforms);
+    for(unsigned int j=0;j<temps.size();j++){
+//      std::cout<<temps[j]->getSpectrumId()<<std::endl;
+      prsms.push_back(temps[j]);
+    }
+  }
+  std::sort(prsms.begin(),prsms.end(),prsm_spectrum);
+  PrSMWriter all_writer(basename(spec_file_)+"."+output_files_);
+  all_writer.writeVector(prsms);
+  all_writer.~PrSMWriter();
+}
+
+} /* namespace prot */
---
+++ src/prsm/prsm_combine.hpp
@@ -0,0 +1,35 @@
+/*
+ * prsm_combine.hpp
+ *
+ *  Created on: Feb 18, 2014
+ *      Author: xunlikun
+ */
+
+#ifndef PRSM_COMBINE_HPP_
+#define PRSM_COMBINE_HPP_
+
+#include <vector>
+#include <string>
+#include "base/proteoform.hpp"
+#include "base/fasta_reader.hpp"
+#include "prsm/prsm.hpp"
+#include "prsm/prsm_writer.hpp"
+
+namespace prot {
+
+class PrSMCombine {
+ public:
+  PrSMCombine(std::string db_file, std::string spec_file,
+              std::vector<std::string> &in_file_exts, std::string out_file);
+  virtual ~PrSMCombine();
+  void process();
+ private:
+  std::string spec_file_;
+  std::string db_file_;
+  std::vector<std::string> input_file_exts_;
+  std::string output_files_;
+};
+typedef std::shared_ptr<PrSMCombine> PrSMCombinePtr;
+} /* namespace prot */
+
+#endif /* PRSM_COMBINE_HPP_ */
