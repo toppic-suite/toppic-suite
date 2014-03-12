150bd078b4241e176d042602b2b5a1ac359d5f4b
Xiaowen <liuxiaowencs@gmail.com>
2014-03-12 16:06:53 -0400

    Testing spectrum 504.

--- src/test/test_console.cpp
+++ src/test/test_console.cpp
@@ -2,6 +2,8 @@
 
 #include "base/base_data.hpp"
 
+#include "prsm/prsm_combine.hpp"
+
 #include "zeroptmsearch/zero_ptm_mng.hpp"
 #include "zeroptmsearch/zero_ptm_search.hpp"
 
@@ -44,11 +46,18 @@ int main(int argc, char* argv[]) {
     prot::PtmProcessor ptm_processor(ptm_mng_ptr);
 		ptm_processor.process();
 
+    std::cout << "Combine prsms " << std::endl;
+		std::vector<std::string> input_exts ;
+		input_exts.push_back("ZERO");
+		input_exts.push_back("PTM");
+    prot::PrSMCombine combine_processor(argv[2], argv[3], input_exts, "RAW_RESULT");
+		combine_processor.process();
+
     std::cout << "EValueConsole 0.1 " << std::endl;
     prot::TdgfMngPtr tdgf_mng_ptr = prot::TdgfMngPtr(new prot::TdgfMng (std::string(argv[1])));
     tdgf_mng_ptr->search_db_file_name_ = argv[2];
     tdgf_mng_ptr->spectrum_file_name_ = argv[3];
-    tdgf_mng_ptr->input_file_ext_ = "ZERO";
+    tdgf_mng_ptr->input_file_ext_ = "RAW_RESULT";
     tdgf_mng_ptr->output_file_ext_ = "EVALUE";
     prot::EValueProcessor processor(tdgf_mng_ptr);
     processor.init();
