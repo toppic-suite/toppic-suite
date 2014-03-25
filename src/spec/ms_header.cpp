5f3ceb38d99dffaedfc7fb254dc7f1a5a240ec46
xunlikun <xunlikun@xlk-vm>
2014-03-25 10:46:19 -0400

    fix scan string

--- src/spec/ms_header.cpp
+++ src/spec/ms_header.cpp
@@ -131,13 +131,15 @@ void MsHeader::appendViewXml(XmlDOMDocument* xml_doc,xercesc::DOMElement* parent
   xercesc::DOMElement* element = xml_doc->createElement("ms_header");
   std::string str = convertToString(id_);
   xml_doc->addElement(element, "id", str.c_str());
-  if (scans_.size() > 0) {
-    str = scans_[0];
-    for (unsigned int i = 1; i < scans_.size(); i++) {
-      str += " " + scans_[i];
-    }
-    xml_doc->addElement(element, "scans", str.c_str());
-  }
+//  if (scans_.size() > 0) {
+//    str = scans_[0];
+//    for (unsigned int i = 1; i < scans_.size(); i++) {
+//      str += " " + scans_[i];
+//    }
+//    xml_doc->addElement(element, "scans", str.c_str());
+//  }
+  str=getScansString();
+  xml_doc->addElement(element, "scans", str.c_str());
   str = convertToString(prec_mono_mz_);
   xml_doc->addElement(element, "precursor_mz", str.c_str());
   str = convertToString(prec_charge_);
