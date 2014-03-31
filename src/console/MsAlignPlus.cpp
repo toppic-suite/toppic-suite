d054c61d48de6f4babfc0862e227ef1e558a8f98
xunlikun <xunlikun@xlk-vm>
2014-03-31 15:37:28 -0400

    modify for html

--- src/console/MsAlignPlus.cpp
+++ src/console/MsAlignPlus.cpp
@@ -26,6 +26,7 @@
 #include "prsm/output_selector.hpp"
 #include "xpp/xml_generator.hpp"
 #include "xpp/transformer.hpp"
+#include "xpp/view_mng.hpp"
 
 using namespace prot;
 using namespace std;
@@ -102,77 +103,98 @@ void split(std::string& s, std::string& delim,std::vector< std::string >& ret)
 	}
 }
 
+void preProcess(map<string,string> arguments){
+  if(arguments["searchType"].compare("TARGET+DECOY")==0){
+      arguments["databaseFileName"]=arguments["databaseFileName"]+arguments["searchType"];
+      ofstream onf;
+      onf.open(arguments["databaseFileName"].c_str(), ios::out);
+      FastaReader reader(arguments["databaseFileName"]);
+//      std::vector<std::string> seq_info = reader.getNextSeq();
+      FastaSeqPtr seq_info = reader.getNextSeq();
+      while (seq_info!=nullptr) {
+        std::string name = seq_info->getName();
+        std::string seq = seq_info->getSeq();
+        std::string temp = seq;
+        std::string decoy_name = "DECOY_" + name;
+        std::string decoy_seq = temp.substr(0, 2);
+        temp = temp.substr(2, temp.length() - 2);
+        for (unsigned int i = temp.length(); i > 0; i--) {
+          int n = std::rand() % i;
+          decoy_seq += temp.substr(n, 1);
+          temp = temp.substr(0, n) + temp.substr(n + 1, temp.length() - n - 1);
+        }
+        onf << decoy_name << std::endl;
+        onf << decoy_seq << std::endl;
+        onf << name << std::endl;
+        onf << seq << std::endl;
+        seq_info = reader.getNextSeq();
+      }
+    }
+}
+
 void MsAlignPipeline(map<string,string> arguments){
 
-//    std::string config_file_name = "conf/configuration.xml";
-//    std::string search_db_file_name = "in/prot.fasta";
-//    std::string spectrum_file_name_ = "in/spectra_one.msalign";
-//    std::string spectrum_file_name_ = "in/spectra.msalign";
+  //pre_process
+  preProcess(arguments);
 
-    prot::ZeroPtmMngPtr mng_ptr = prot::ZeroPtmMngPtr(new prot::ZeroPtmMng (arguments));
-
-//    mng_ptr->search_db_file_name_ = search_db_file_name;
-//    mng_ptr->spectrum_file_name_ = spectrum_file_name_;
-    mng_ptr->output_file_ext_ = "ZERO_PTM_SEARCH";
-    prot::zeroPtmSearchProcess(mng_ptr);
-
-    PtmFastFilterMngPtr fast_filter_mng = PtmFastFilterMngPtr(new PtmFastFilterMng(arguments));
-//    fast_filter_mng->search_db_file_name_ = search_db_file_name;
-//    fast_filter_mng->spectrum_file_name_ = spectrum_file_name_;
-    PtmFastFilterProcessorPtr processorb = PtmFastFilterProcessorPtr(new PtmFastFilterProcessor(fast_filter_mng));
-    processorb->process();
-
-    PtmMngPtr ptm_search_mng = PtmMngPtr(new PtmMng(arguments));
-//    ptm_search_mng->search_db_file_name_ = search_db_file_name;
-//    ptm_search_mng->spectrum_file_name_ = spectrum_file_name_;
-    ptm_search_mng->input_file_ext_ ="_COMBINED";
-    ptm_search_mng->output_file_ext_="PTM_SEARCH_RESULT";
-    PtmProcessorPtr processor = PtmProcessorPtr(new PtmProcessor(ptm_search_mng));
-    processor->process();
-
-    std::vector<std::string> input_exts ;
-    input_exts.push_back("PTM_SEARCH_RESULT");
-    input_exts.push_back("ZERO_PTM_SEARCH");
-    PrSMCombinePtr combine = PrSMCombinePtr(new PrSMCombine(arguments,
-                                                            input_exts,
-                                                            "RAW_SEARCH_RESULT"));
-    combine->process();
-
-
-    prot::TdgfMngPtr tdgf_mng = prot::TdgfMngPtr(new prot::TdgfMng (arguments));
-//    tdgf_mng->search_db_file_name_ = search_db_file_name;
-//    tdgf_mng->spectrum_file_name_ = spectrum_file_name_;
-    tdgf_mng->input_file_ext_ = "RAW_SEARCH_RESULT";
-    tdgf_mng->output_file_ext_ = "EVALUED_RESULT";
-    EValueProcessor evalue_processor(tdgf_mng);
-    evalue_processor.init();
-    /* compute E-value for a set of prsms each run */
-    evalue_processor.process(false);
-
-    PrSMSelector selector = PrSMSelector(arguments,
-                                         "EVALUED_RESULT",
-                                         "TOP_RESULT",
-                                         1,
-                                         "e");
-    selector.process();
-
-    OutputSelector output_selector = OutputSelector(arguments,
-                                                    "TOP_RESULT",
-                                                    "OUTPUT_RESULT");
-    output_selector.process();
-
-    TableWriterPtr table_out = TableWriterPtr(new TableWriter(arguments,"OUTPUT_RESULT","OUTPUT_TABLE"));
-    table_out->write();
-
-
-
-
-//    XmlGenerator xml_gene = XmlGenerator(spectrum_file_name_,search_db_file_name,"TOP_RESULT");
-//    xml_gene.process();
-
-//    TransformerPtr trans = TransformerPtr(new Transformer());
-//    trans->trans();
+  prot::ZeroPtmMngPtr mng_ptr = prot::ZeroPtmMngPtr(new prot::ZeroPtmMng (arguments));
+//  mng_ptr->output_file_ext_ = "ZERO_PTM_SEARCH";
+//  prot::zeroPtmSearchProcess(mng_ptr);
 //
+//  PtmFastFilterMngPtr fast_filter_mng = PtmFastFilterMngPtr(new PtmFastFilterMng(arguments));
+//  PtmFastFilterProcessorPtr processorb = PtmFastFilterProcessorPtr(new PtmFastFilterProcessor(fast_filter_mng));
+//  processorb->process();
+//
+//  PtmMngPtr ptm_search_mng = PtmMngPtr(new PtmMng(arguments));
+//  ptm_search_mng->input_file_ext_ ="_COMBINED";
+//  ptm_search_mng->output_file_ext_="PTM_SEARCH_RESULT";
+//  PtmProcessorPtr processor = PtmProcessorPtr(new PtmProcessor(ptm_search_mng));
+//  processor->process();
+//
+//  std::vector<std::string> input_exts ;
+//  input_exts.push_back("PTM_SEARCH_RESULT");
+//  input_exts.push_back("ZERO_PTM_SEARCH");
+//  PrSMCombinePtr combine = PrSMCombinePtr(new PrSMCombine(arguments,
+//                                                          input_exts,
+//                                                          "RAW_SEARCH_RESULT"));
+//  combine->process();
+//
+//
+//  prot::TdgfMngPtr tdgf_mng = prot::TdgfMngPtr(new prot::TdgfMng (arguments));
+//  tdgf_mng->input_file_ext_ = "RAW_SEARCH_RESULT";
+//  tdgf_mng->output_file_ext_ = "EVALUED_RESULT";
+//  EValueProcessor evalue_processor(tdgf_mng);
+//  evalue_processor.init();
+//  /* compute E-value for a set of prsms each run */
+//  evalue_processor.process(false);
+//
+//  PrSMSelector selector = PrSMSelector(arguments,
+//                                       "EVALUED_RESULT",
+//                                       "TOP_RESULT",
+//                                       1,
+//                                       "e");
+//  selector.process();
+//
+//  if(arguments["searchType"].compare("TARGET+DECOY")==0){
+//
+//  }
+//
+//  OutputSelector output_selector = OutputSelector(arguments,
+//                                                  "TOP_RESULT",
+//                                                  "OUTPUT_RESULT");
+//  output_selector.process();
+//
+//  TableWriterPtr table_out = TableWriterPtr(new TableWriter(arguments,"OUTPUT_RESULT","OUTPUT_TABLE"));
+//  table_out->write();
+
+
+
+    XmlGenerator xml_gene = XmlGenerator(arguments,"OUTPUT_RESULT");
+    xml_gene.process();
+
+    TransformerPtr trans = TransformerPtr(new Transformer());
+    trans->trans();
+
 }
 
 int main(int argc, char* argv[]){
