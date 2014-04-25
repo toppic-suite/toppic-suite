0b31a118ab6639692fec9e27b018279a9e41fa22
xunlikun <xunlikun@xlk-vm>
2014-04-25 16:55:46 -0400

    Issue #59

--- src/console/MsAlignPlus.cpp
+++ src/console/MsAlignPlus.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 
+#include "base/command.hpp"
 #include "base/fasta_reader.hpp"
 #include "prsm/prsm_writer.hpp"
 #include "prsm/prsm_combine.hpp"
@@ -29,7 +30,6 @@
 #include "xpp/transformer.hpp"
 #include "xpp/view_mng.hpp"
 #include "xpp/folder_file.hpp"
-#include "console/command.hpp"
 
 using namespace prot;
 using namespace std;
@@ -65,78 +65,78 @@ void preProcess(map<string,string> arguments){
 
 void MsAlignPipeline(map<string,string> arguments){
 
-  //pre_process
-//  preProcess(arguments);
-//
-//  prot::ZeroPtmMngPtr mng_ptr = prot::ZeroPtmMngPtr(new prot::ZeroPtmMng (arguments));
-//  mng_ptr->output_file_ext_ = "ZERO_PTM_SEARCH";
-//  prot::zeroPtmSearchProcess(mng_ptr);
-//
-//  PtmFastFilterMngPtr fast_filter_mng = PtmFastFilterMngPtr(new PtmFastFilterMng(arguments));
-//  PtmFastFilterProcessorPtr processorb = PtmFastFilterProcessorPtr(new PtmFastFilterProcessor(fast_filter_mng));
-//  processorb->process();
-//
-//  PtmMngPtr ptm_search_mng = PtmMngPtr(new PtmMng(arguments));
-//  ptm_search_mng->input_file_ext_ ="_COMBINED";
-//  ptm_search_mng->output_file_ext_="PTM_SEARCH_RESULT";
-//  PtmProcessorPtr processor = PtmProcessorPtr(new PtmProcessor(ptm_search_mng));
-//  processor->process();
-//
-//  std::vector<std::string> input_exts ;
-//  input_exts.push_back("PTM_SEARCH_RESULT");
-//  input_exts.push_back("ZERO_PTM_SEARCH");
-//  PrSMCombinePtr combine = PrSMCombinePtr(new PrSMCombine(arguments,
-//                                                          input_exts,
-//                                                          "RAW_SEARCH_RESULT"));
-//  combine->process();
-//
-//
-//  prot::TdgfMngPtr tdgf_mng = prot::TdgfMngPtr(new prot::TdgfMng (arguments));
-//  tdgf_mng->input_file_ext_ = "RAW_SEARCH_RESULT";
-//  tdgf_mng->output_file_ext_ = "EVALUED_RESULT";
-//  EValueProcessor evalue_processor(tdgf_mng);
-//  evalue_processor.init();
-//  /* compute E-value for a set of prsms each run */
-//  evalue_processor.process(false);
-//
-//  if(arguments["searchType"].compare("TARGET+DECOY")==0){
-//	PrSMSelector selector = PrSMSelector(arguments,
-//	                                     "EVALUED_RESULT",
-//	                                     "TOP",
-//	                                     1);
-//	selector.process();
-//
-//    prot::PrSMFdr fdr(arguments, "TOP", "TOP_RESULT");
-//    fdr.process();
-//  }
-//  else{
-//    PrSMSelector selector = PrSMSelector(arguments,
-//                                       "EVALUED_RESULT",
-//                                       "TOP_RESULT",
-//                                       1);
-//    selector.process();
-//  }
-//
-//  OutputSelector output_selector = OutputSelector(arguments,
-//                                                  "TOP_RESULT",
-//                                                  "OUTPUT_RESULT");
-//  output_selector.process();
-//
-//  TableWriterPtr table_out = TableWriterPtr(new TableWriter(arguments,"OUTPUT_RESULT","OUTPUT_TABLE"));
-//  table_out->write();
+//  pre_process
+  preProcess(arguments);
+
+  prot::ZeroPtmMngPtr mng_ptr = prot::ZeroPtmMngPtr(new prot::ZeroPtmMng (arguments));
+  mng_ptr->output_file_ext_ = "ZERO_PTM_SEARCH";
+  prot::zeroPtmSearchProcess(mng_ptr);
+
+  PtmFastFilterMngPtr fast_filter_mng = PtmFastFilterMngPtr(new PtmFastFilterMng(arguments));
+  PtmFastFilterProcessorPtr processorb = PtmFastFilterProcessorPtr(new PtmFastFilterProcessor(fast_filter_mng));
+  processorb->process();
+
+  PtmMngPtr ptm_search_mng = PtmMngPtr(new PtmMng(arguments));
+  ptm_search_mng->input_file_ext_ ="_COMBINED";
+  ptm_search_mng->output_file_ext_="PTM_SEARCH_RESULT";
+  PtmProcessorPtr processor = PtmProcessorPtr(new PtmProcessor(ptm_search_mng));
+  processor->process();
+
+  std::vector<std::string> input_exts ;
+  input_exts.push_back("PTM_SEARCH_RESULT");
+  input_exts.push_back("ZERO_PTM_SEARCH");
+  PrSMCombinePtr combine = PrSMCombinePtr(new PrSMCombine(arguments,
+                                                          input_exts,
+                                                          "RAW_SEARCH_RESULT"));
+  combine->process();
 
 
-  createFolder("xml/species");
-  createFolder("xml/prsms");
-  createFolder("xml/proteins");
+  prot::TdgfMngPtr tdgf_mng = prot::TdgfMngPtr(new prot::TdgfMng (arguments));
+  tdgf_mng->input_file_ext_ = "RAW_SEARCH_RESULT";
+  tdgf_mng->output_file_ext_ = "EVALUED_RESULT";
+  EValueProcessor evalue_processor(tdgf_mng);
+  evalue_processor.init();
+  /* compute E-value for a set of prsms each run */
+  evalue_processor.process(false);
+
+  if(arguments["searchType"].compare("TARGET+DECOY")==0){
+	PrSMSelector selector = PrSMSelector(arguments,
+	                                     "EVALUED_RESULT",
+	                                     "TOP",
+	                                     1);
+	selector.process();
+
+    prot::PrSMFdr fdr(arguments, "TOP", "TOP_RESULT");
+    fdr.process();
+  }
+  else{
+    PrSMSelector selector = PrSMSelector(arguments,
+                                       "EVALUED_RESULT",
+                                       "TOP_RESULT",
+                                       1);
+    selector.process();
+  }
+
+  OutputSelector output_selector = OutputSelector(arguments,
+                                                  "TOP_RESULT",
+                                                  "OUTPUT_RESULT");
+  output_selector.process();
+
+  TableWriterPtr table_out = TableWriterPtr(new TableWriter(arguments,"OUTPUT_RESULT","OUTPUT_TABLE"));
+  table_out->write();
+
+
+//  createFolder("xml/species");
+//  createFolder("xml/prsms");
+//  createFolder("xml/proteins");
   XmlGenerator xml_gene = XmlGenerator(arguments,"OUTPUT_RESULT");
   xml_gene.process();
 
-  createFolder("html/species");
-  createFolder("html/prsms");
-  createFolder("html/proteins");
-  copyFile("etc/FreeMono.ttf","html/FreeMono.ttf",true);
-  copyFile("etc/sorttable.js","html/sorttable.js",true);
+//  createFolder("html/species");
+//  createFolder("html/prsms");
+//  createFolder("html/proteins");
+//  copyFile("etc/FreeMono.ttf","html/FreeMono.ttf",true);
+//  copyFile("etc/sorttable.js","html/sorttable.js",true);
 
     TransformerPtr trans = TransformerPtr(new Transformer());
     trans->trans();
@@ -146,11 +146,11 @@ void MsAlignPipeline(map<string,string> arguments){
 int main(int argc, char* argv[]){
 //  std::cout<<argv[0]<<std::endl;
   cmdPtr cmd = cmdPtr(new command());
-  std::string command_path = argv[0];
-  std::string run_path = getExePath(command_path);
-  if(run_path.compare("########")==0){
-    LOG_ERROR("Can't find the configuration folder: conf or "+command_path+"conf or &PATH/conf;Please check if the folder exist!");
-  }
+  SystemInfo::initSystemInfo(argv[0]);
+
+  initBaseData();
+
+  cmd->setArgumentsWithConfigFile(SystemInfo::getExeFilePath()+"/conf/arguments.xml");
 
   for(int i=1;i<argc;i++){
     int flag=0;
@@ -193,12 +193,58 @@ int main(int argc, char* argv[]){
           }
       }
 
-      std::map<std::string,std::string>::iterator it;
-      for(it=cmd->getArgument().begin();it!=cmd->getArgument().end();++it){
-        cout<<"key:"<<it->first<<" -> value:"<<it->second<<endl;
-      }
+//        std::cout<<cmd->getArgument()["argumentFileName"]<<endl;
+//        std::cout<<cmd->getArgument()["configuration"]<<endl;
+//        std::cout<<cmd->getArgument()["databaseFileName"] <<endl;
+//        std::cout<<cmd->getArgument()["spectrumFileName"]<<endl;
+//        std::cout<<cmd->getArgument()["activation"] <<endl;
+//        std::cout<<cmd->getArgument()["searchType"] <<endl;
+//        std::cout<<cmd->getArgument()["cysteineProtection"]<<endl;
+//        std::cout<<cmd->getArgument()["shiftNumber"] <<endl;
+//        std::cout<<cmd->getArgument()["errorTolerance"] <<endl;
+//        std::cout<<cmd->getArgument()["cutoffType"] <<endl;
+//        std::cout<<cmd->getArgument()["cutoff"]<<endl;
+//        std::cout<<cmd->getArgument()["allProtMode"] <<endl;
 
-//      MsAlignPipeline(arguments);
+
+        std::cout<<SystemInfo::getExeFilePath()<<std::endl;
+
+        std::cout<<directory(cmd->getArgument()["spectrumFileName"])<<std::endl;
+
+        std::string xml_path=SystemInfo::getExeFilePath()+"/xml/";
+        std::string html_path=directory(cmd->getArgument()["spectrumFileName"])+"html/";
+
+        std::cout<<"cp "+SystemInfo::getExeFilePath()+"/etc/*.* "+html_path<<std::endl;
+
+        if(SystemInfo::getOSString().compare("uni")==0){
+          runCommand("mkdir "+xml_path);
+          runCommand("mkdir "+xml_path+"species");
+          runCommand("mkdir "+xml_path+"prsms");
+          runCommand("mkdir "+xml_path+"proteins");
+
+          runCommand("mkdir "+html_path);
+          runCommand("mkdir "+html_path+"species");
+          runCommand("mkdir "+html_path+"prsms");
+          runCommand("mkdir "+html_path+"proteins");
+
+          runCommand("cp "+SystemInfo::getExeFilePath()+"/etc/*.* "+html_path);
+        }
+        else{
+          runCommand("md " + xml_path);
+          runCommand("md " + xml_path + "species");
+          runCommand("md " + xml_path + "prsms");
+          runCommand("md " + xml_path + "proteins");
+
+          runCommand("md " + html_path);
+          runCommand("md " + html_path + "species");
+          runCommand("md " + html_path + "prsms");
+          runCommand("md " + html_path + "proteins");
+
+          runCommand("copy " + SystemInfo::getExeFilePath() + "/etc/*.* " + html_path);
+        }
+
+
+      MsAlignPipeline(cmd->getArgument());
 
       return 0;
 }
