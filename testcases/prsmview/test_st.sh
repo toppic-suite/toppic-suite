5df037df60c2be38b2d8596a4678bdd06a9ec4ae
Xiaowen <liuxiaowencs@gmail.com>
2014-09-12 11:51:45 -0400

    Added prsmview/test_st.sh

--- testcases/prsmview/test_st.sh
+++ testcases/prsmview/test_st.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir tmp
-cd tmp
-ln -s  ../../data/st.fasta ./ 
-ln -s  ../../data/st_no_digestion.msalign ./
-cd ..
+#mkdir tmp
+#cd tmp
+#ln -s  ../../data/st.fasta ./ 
+#ln -s  ../../data/st_no_digestion.msalign ./
+#cd ..
 ../../bin/test_prsm_view tmp/st.fasta tmp/st_no_digestion.msalign -c C57 -d -t FDR| tee tmp/log 
