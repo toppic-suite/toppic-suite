4ab1fa64c06ce7d60b33685453ef1b1a6d98d7cb
xiaowen <liuxiaowencs@gmail.com>
2014-11-08 20:13:43 -0500

    Added db_block

---
+++ src/base/db_block.hpp
@@ -0,0 +1,30 @@
+#ifndef PROT_DB_BLOCK_HPP_
+#define PROT_DB_BLOCK_HPP_
+
+#include <memory>
+#include <vector>
+
+namespace prot {
+
+class DbBlock {
+ public:
+  DbBlock(int block_index, int seq_index);
+
+  int getBlockIdx() {return block_index_;}
+  int getSeqIdx() {return seq_index_;}
+
+ private:
+  int block_index_;
+  int seq_index_;
+};
+
+typedef std::shared_ptr<DbBlock> DbBlockPtr;
+typedef std::vector<DbBlockPtr> DbBlockPtrVec;
+
+DbBlockPtrVec readDbBlockIndex(const std::string &index_file_name);
+
+void generateDbBlockIndex(const std::string &db_file_name, 
+                          int block_size);
+
+}
+#endif
