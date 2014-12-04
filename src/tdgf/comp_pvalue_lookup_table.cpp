022b4ad6e8fbc7c22d1b4acf242b90c24faf7d25
Qiang Kou <qkou@umail.iu.edu>
2014-12-04 17:46:26 -0500

    pvalue computation using table

--- src/tdgf/comp_pvalue_lookup_table.cpp
+++ src/tdgf/comp_pvalue_lookup_table.cpp
@@ -17,8 +17,11 @@ CompPValueLookupTable::CompPValueLookupTable(TdgfMngPtr mng_ptr) {
 void CompPValueLookupTable::initTable() {
   // add init table
   int ppo = mng_ptr_->prsm_para_ptr_->getErrorTolerance();
-
   LOG_DEBUG("ppo " << ppo);
+
+  memset(ptm0_, 0, sizeof(ptm0_[0][0]) * 41 * 20);
+  memset(ptm1_, 0, sizeof(ptm0_[0][0]) * 41 * 20);
+  memset(ptm2_, 0, sizeof(ptm0_[0][0]) * 41 * 20);
 
   std::string line;
   std::vector<std::string> strs;
@@ -65,27 +68,65 @@ void CompPValueLookupTable::initTable() {
 
   input_.close();
   LOG_DEBUG("table initialized");
+
 }
 
-double CompPValueLookupTable::compProb(int match_frag_num,
+double CompPValueLookupTable::compProb(int peak_num, int match_frag_num,
                                        int unexpected_shift_num) {
   // add implementation.
-  return 1.0;
+  std::vector<int> idx = getFourIndex(peak_num, match_frag_num);
+  double res = 0;
+
+  int x1 = idx[0], x2 = idx[1], y1 = idx[2], y2 = idx[3];
+
+  double p11, p12, p21, p22;
+
+  if (unexpected_shift_num == 0) {
+    p11 = ptm0_[x1][y1];
+    p12 = ptm0_[x1][y2];
+    p21 = ptm0_[x2][y1];
+    p22 = ptm0_[x2][y2];
+  } else if (unexpected_shift_num == 1) {
+    p11 = ptm1_[x1][y1];
+    p12 = ptm1_[x1][y2];
+    p21 = ptm1_[x2][y1];
+    p22 = ptm1_[x2][y2];
+  } else {
+    p11 = ptm2_[x1][y1];
+    p12 = ptm2_[x1][y2];
+    p21 = ptm2_[x2][y1];
+    p22 = ptm2_[x2][y2];
+  }
+
+  x1 = getPeakNumFromIndex(idx[0]);
+  x2 = getPeakNumFromIndex(idx[1]);
+  y1 = 5 * (idx[2] + 1);
+  y2 = 5 * (idx[3] + 1);
+
+  res = ((x2 - peak_num) * (y2 - match_frag_num) * p11
+      + (peak_num - x1) * (y2 - match_frag_num) * p21
+      + (x2 - peak_num) * (match_frag_num - y1) * p12
+      + (peak_num - x1) * (match_frag_num - y1) * p22)
+      / ((x2 - x1) * (y2 - y1));
+
+  LOG_DEBUG("prob " << res);
+
+  return res;
 }
 
 /* set alignment */
 void CompPValueLookupTable::process(DeconvMsPtr deconv_ms_ptr,
                                     PrsmPtrVec &prsm_ptrs) {
 
-  int peak_num = deconv_ms_ptr->size();
   double tolerance = deconv_ms_ptr->getHeaderPtr()->getErrorTolerance();
+  int peak_num = deconv_ms_ptr->size();
   for (size_t i = 0; i < prsm_ptrs.size(); i++) {
     double refine_prec_mass = prsm_ptrs[i]->getAdjustedPrecMass();
     int match_frag_num = prsm_ptrs[i]->getMatchFragNum();
     int unexpected_shift_num = prsm_ptrs[i]->getProteoformPtr()
         ->getUnexpectedChangeNum();
 
-    double prot_prob = compProb(match_frag_num, unexpected_shift_num);
+    double prot_prob = compProb(peak_num, match_frag_num, unexpected_shift_num);
 
     SemiAlignTypePtr type_ptr = prsm_ptrs[i]->getProteoformPtr()
         ->getSemiAlignType();
@@ -98,6 +139,44 @@ void CompPValueLookupTable::process(DeconvMsPtr deconv_ms_ptr,
     prsm_ptrs[i]->setProbPtr(prob_ptr);
 
   }
+}
+
+bool CompPValueLookupTable::inTable(DeconvMsPtr deconv_ms_ptr,
+                                    PrsmPtrVec &prsm_ptrs) {
+
+  int peak_num = deconv_ms_ptr->size();
+
+  if (peak_num > 500 || peak_num < 10)
+    return false;
+
+  for (size_t i = 0; i < prsm_ptrs.size(); i++) {
+    int match_frag_num = prsm_ptrs[i]->getMatchFragNum();
+
+    if (match_frag_num > 100)
+      return false;
+
+    std::vector<int> idx = getFourIndex(peak_num, match_frag_num);
+
+    int unexpected_shift_num = prsm_ptrs[i]->getProteoformPtr()
+        ->getUnexpectedChangeNum();
+
+    if (unexpected_shift_num == 0) {
+      if (ptm0_[idx[0]][idx[2]] == 0 || ptm0_[idx[0]][idx[3]] == 0
+          || ptm0_[idx[1]][idx[2]] == 0 || ptm0_[idx[1]][idx[3]] == 0)
+        return false;
+    } else if (unexpected_shift_num == 1) {
+      if (ptm1_[idx[0]][idx[2]] == 0 || ptm1_[idx[0]][idx[3]] == 0
+          || ptm1_[idx[1]][idx[2]] == 0 || ptm1_[idx[1]][idx[3]] == 0)
+        return false;
+    } else {
+      if (ptm2_[idx[0]][idx[2]] == 0 || ptm2_[idx[0]][idx[3]] == 0
+          || ptm2_[idx[1]][idx[2]] == 0 || ptm2_[idx[1]][idx[3]] == 0)
+        return false;
+    }
+  }
+
+  return true;
+
 }
 
 int getPeakIndex(int p) {
@@ -151,6 +230,64 @@ int getFragIndex(int i) {
     return k;
   else
     return k - 1;
+}
+
+std::vector<int> getFourIndex(int peak_num, int frag_num) {
+  std::vector<int> idx(4);
+
+  int k;
+
+  if (peak_num <= 10) {
+    idx[0] = 0;
+    idx[1] = 1;
+  } else if (peak_num <= 100) {
+    k = peak_num / 5;
+
+    idx[0] = k - 2;
+    idx[1] = k - 1;
+  } else if (peak_num <= 200) {
+    peak_num = peak_num - 100;
+    k = peak_num / 10;
+    idx[0] = k + 18;
+    idx[1] = k + 19;
+  } else if (peak_num <= 400) {
+    peak_num = peak_num - 200;
+    k = peak_num / 20;
+    idx[0] = k + 28;
+    idx[1] = k + 29;
+  } else {
+    if (peak_num <= 450) {
+      idx[0] = 38;
+      idx[1] = 39;
+    } else {
+      idx[0] = 39;
+      idx[1] = 40;
+    }
+  }
+
+  if (frag_num <= 5) {
+    idx[2] = 0;
+    idx[3] = 1;
+  } else {
+
+    k = frag_num / 5;
+
+    idx[2] = k - 1;
+    idx[3] = k;
+  }
+  return idx;
+}
+
+int getPeakNumFromIndex(int idx) {
+  if (idx <= 18) {
+    return 5 * (idx + 2);
+  } else if (idx <= 28) {
+    return 100 + 10 * (idx - 18);
+  } else if (idx <= 38) {
+    return 200 + 20 * (idx - 28);
+  } else {
+    return 400 + 50 * (idx - 38);
+  }
 }
 
 }
