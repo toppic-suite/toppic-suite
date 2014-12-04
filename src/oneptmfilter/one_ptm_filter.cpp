71442213d8e4272597daad512caa340849b07dcd
Xiaowen <liuxiaowencs@gmail.com>
2014-12-03 20:57:14 -0500

    Fixed a bug in one_ptm_filter

--- src/oneptmfilter/one_ptm_filter.cpp
+++ src/oneptmfilter/one_ptm_filter.cpp
@@ -11,7 +11,8 @@ OnePtmFilter::OnePtmFilter(const ProteoformPtrVec &proteo_ptrs,
   index_ptr_ = OnePtmCompShiftPtr(new OnePtmCompShift(proteo_ptrs, mng_ptr));
 }
 
-SimplePrsmPtrVec OnePtmFilter::getBestMatch(PrmMsPtr ms_ptr){
+/*
+void OnePtmFilter::compBestMatch(PrmMsPtr ms_ptr){
   SimplePrsmPtrVec match_ptrs = compute(ms_ptr);
   SimplePrsmPtrVec unique_match_ptrs = getUniqueMatches(match_ptrs);
   std::sort(unique_match_ptrs.begin(), unique_match_ptrs.end(),simplePrsmDown);
@@ -31,22 +32,46 @@ SimplePrsmPtrVec OnePtmFilter::getBestMatch(PrmMsPtr ms_ptr){
   }
   return result_ptrs;
 }
+*/
 
-inline SimplePrsmPtrVec OnePtmFilter::compute(PrmMsPtr ms_ptr){
+void OnePtmFilter::computeBestMatch(PrmMsPtr ms_ptr){
   std::pair<std::vector<int>, std::vector<int>> mass_errors 
       = getIntMassErrorList(ms_ptr, mng_ptr_->ptm_fast_filter_scale_, true, false);
-  SimplePrsmPtrVec match_ptrs;
   //LOG_DEBUG("start convolution");
-  std::vector<std::pair<int,int>> results 
-      =index_ptr_->compConvolution(mass_errors.first, mass_errors.second, 
-                                   mng_ptr_->one_ptm_filter_result_num_);
-  for(size_t i =0; i <results.size(); i++) {
-    match_ptrs.push_back(
+  index_ptr_->compConvolution(mass_errors.first, mass_errors.second, 
+                              mng_ptr_->one_ptm_filter_result_num_);
+  std::vector<std::pair<int,int>> comp_scores = index_ptr_->getTopCompProteoScores();
+  comp_match_ptrs_.clear();
+  for (size_t i = 0; i < comp_scores.size(); i++) {
+    comp_match_ptrs_.push_back( 
         SimplePrsmPtr(new SimplePrsm(ms_ptr->getHeaderPtr(),
-                                     proteo_ptrs_[results[i].first],
-                                     results[i].second)));
+                                     proteo_ptrs_[comp_scores[i].first], comp_scores[i].second)));
   }
-  return match_ptrs;
+
+  std::vector<std::pair<int,int>> pref_scores = index_ptr_->getTopPrefProteoScores();
+  pref_match_ptrs_.clear();
+  for (size_t i = 0; i < pref_scores.size(); i++) {
+    pref_match_ptrs_.push_back( 
+        SimplePrsmPtr(new SimplePrsm(ms_ptr->getHeaderPtr(),
+                                     proteo_ptrs_[pref_scores[i].first], pref_scores[i].second)));
+  }
+
+  std::vector<std::pair<int,int>> suff_scores = index_ptr_->getTopSuffProteoScores();
+  suff_match_ptrs_.clear();
+  for (size_t i = 0; i < suff_scores.size(); i++) {
+    suff_match_ptrs_.push_back( 
+        SimplePrsmPtr(new SimplePrsm(ms_ptr->getHeaderPtr(),
+                                     proteo_ptrs_[suff_scores[i].first], suff_scores[i].second)));
+  }
+
+  std::vector<std::pair<int,int>> internal_scores = index_ptr_->getTopInternalProteoScores();
+  internal_match_ptrs_.clear();
+  for (size_t i = 0; i < internal_scores.size(); i++) {
+    internal_match_ptrs_.push_back( 
+        SimplePrsmPtr(new SimplePrsm(ms_ptr->getHeaderPtr(),
+                                     proteo_ptrs_[internal_scores[i].first], internal_scores[i].second)));
+  }
+
 }
 
 
--- src/oneptmfilter/one_ptm_filter_processor.cpp
+++ src/oneptmfilter/one_ptm_filter_processor.cpp
@@ -31,10 +31,35 @@ void OnePtmFilterProcessor::process(){
   std::string sp_file_name = mng_ptr_->prsm_para_ptr_->getSpectrumFileName();
   int block_num = db_block_ptr_vec.size();
 
-  SimplePrsmStrCombinePtr combine_ptr(new SimplePrsmStrCombine(sp_file_name, mng_ptr_->output_file_ext_,
-                                                               block_num, mng_ptr_->output_file_ext_, 
-                                                               mng_ptr_->one_ptm_filter_result_num_));
-  combine_ptr->process();
+  SimplePrsmStrCombine comp_combine(sp_file_name, mng_ptr_->output_file_ext_ + "_COMPLETE",
+                                    block_num, mng_ptr_->output_file_ext_ + "_COMPLETE", 
+                                    mng_ptr_->one_ptm_filter_result_num_/4 );
+  comp_combine.process();
+
+  SimplePrsmStrCombine pref_combine(sp_file_name, mng_ptr_->output_file_ext_ + "_PREFIX",
+                                    block_num, mng_ptr_->output_file_ext_ + "_PREFIX", 
+                                    mng_ptr_->one_ptm_filter_result_num_/4 );
+  pref_combine.process();
+
+  SimplePrsmStrCombine suff_combine(sp_file_name, mng_ptr_->output_file_ext_ + "_SUFFIX",
+                                    block_num, mng_ptr_->output_file_ext_ + "_SUFFIX", 
+                                    mng_ptr_->one_ptm_filter_result_num_/4 );
+  suff_combine.process();
+
+  SimplePrsmStrCombine internal_combine(sp_file_name, mng_ptr_->output_file_ext_ + "_INTERNAL",
+                                        block_num, mng_ptr_->output_file_ext_ + "_INTERNAL", 
+                                        mng_ptr_->one_ptm_filter_result_num_/4 );
+  internal_combine.process();
+
+  std::vector<std::string> input_file_exts;
+  input_file_exts.push_back(mng_ptr_->output_file_ext_ + "_COMPLETE");
+  input_file_exts.push_back(mng_ptr_->output_file_ext_ + "_PREFIX");
+  input_file_exts.push_back(mng_ptr_->output_file_ext_ + "_SUFFIX");
+  input_file_exts.push_back(mng_ptr_->output_file_ext_ + "_INTERNAL");
+
+  SimplePrsmStrCombine combine(sp_file_name, input_file_exts, mng_ptr_->output_file_ext_, 
+                               mng_ptr_->one_ptm_filter_result_num_);
+  combine.process();
 
   std::cout << "One PTM filtering: combining blocks finished." << std::endl; 
 }
@@ -51,10 +76,14 @@ void OnePtmFilterProcessor::processBlock(DbBlockPtr block_ptr, int total_block_n
 
   MsAlignReader reader(prsm_para_ptr->getSpectrumFileName());
   std::string output_file_name = basename(prsm_para_ptr->getSpectrumFileName())
-      + "." + mng_ptr_->output_file_ext_+"_"+ std::to_string(block_ptr->getBlockIdx());
+      + "." + mng_ptr_->output_file_ext_;
+  std::string block_str = std::to_string(block_ptr->getBlockIdx());
     
+  SimplePrsmWriter comp_writer(output_file_name + "_COMPLETE_" + block_str);
+  SimplePrsmWriter pref_writer(output_file_name + "_PREFIX_" + block_str);
+  SimplePrsmWriter suff_writer(output_file_name + "_SUFFIX_" + block_str);
+  SimplePrsmWriter internal_writer(output_file_name + "_INTERNAL_" + block_str);
       
-  SimplePrsmWriter writer(output_file_name);
   DeconvMsPtr deconv_ms_ptr;
   int spectrum_num = getSpNum (prsm_para_ptr->getSpectrumFileName());
   int cnt = 0;
@@ -64,8 +93,11 @@ void OnePtmFilterProcessor::processBlock(DbBlockPtr block_ptr, int total_block_n
                                                      prsm_para_ptr->getSpParaPtr());
     if(spectrum_set_ptr != nullptr){
       PrmMsPtr ms_ptr = spectrum_set_ptr->getMsTwoPtr();
-      SimplePrsmPtrVec match_ptrs = filter_ptr->getBestMatch(ms_ptr);
-      writer.write(match_ptrs);
+      filter_ptr->computeBestMatch(ms_ptr);
+      comp_writer.write(filter_ptr->getCompMatchPtrs());
+      pref_writer.write(filter_ptr->getPrefMatchPtrs());
+      suff_writer.write(filter_ptr->getSuffMatchPtrs());
+      internal_writer.write(filter_ptr->getInternalMatchPtrs());
     }
     
     WebLog::percent_log(0.128 + (double) (cnt + spectrum_num * block_ptr->getBlockIdx())
@@ -76,8 +108,10 @@ void OnePtmFilterProcessor::processBlock(DbBlockPtr block_ptr, int total_block_n
   }
   std::cout << std::endl;
   reader.close();
-  writer.close();
-
+  comp_writer.close();
+  pref_writer.close();
+  suff_writer.close();
+  internal_writer.close();
 }
 
 } /* namespace prot */
