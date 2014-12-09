708a0ac4ab9f44c0d4d2a787cf9d2c5ff0f3b602
Xiaowen <liuxiaowencs@gmail.com>
2014-12-09 15:51:02 -0500

    Fixed a bug in msalign_reader and completed validation for zero_ptm_search for multiple spectra

--- src/tdgf/comp_pvalue_lookup_table.cpp
+++ src/tdgf/comp_pvalue_lookup_table.cpp
@@ -123,7 +123,7 @@ double CompPValueLookupTable::compProb(int peak_num, int match_frag_num,
 
 /* set alignment */
 void CompPValueLookupTable::process(const DeconvMsPtrVec &deconv_ms_ptr_vec, PrsmPtrVec &prsm_ptrs) {
-  int ppo = mng_ptr_->prsm_para_ptr_->getSpParaPtr()->getPeakTolerancePtr()->getPpo();
+  //int ppo = mng_ptr_->prsm_para_ptr_->getSpParaPtr()->getPeakTolerancePtr()->getPpo();
   int peak_num = 0; 
   for (size_t i = 0; i < deconv_ms_ptr_vec.size(); i++) {
     peak_num += deconv_ms_ptr_vec[i]->size();
