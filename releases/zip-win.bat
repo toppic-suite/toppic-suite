606a23ccf27735ddac31241ef95625b283073acf
unknown <xwliu@in-info-wk304xl.ads.iu.edu>
2015-01-08 17:34:46 -0500

    Added zin-win.bat

---
+++ releases/zip-win.bat
@@ -0,0 +1,9 @@
+mkdir toppic-win-%1
+cp ..\bin\toppic.exe toppic-win-%1 
+cp ..\LICENSE toppic-win-%1
+cp -r ..\toppic_resources toppic-win-%1
+cp C:\mingw64\bin\libgcc_s_seh-1.dll toppic-win-%1
+cp C:\mingw64\bin\libstdc++-6.dll toppic-win-%1
+cp C:\mingw64\bin\libwinpthread-1.dll toppic-win-%1
+
+
