8968bf671cebd8c8e35b52a1fb9b7a0352b898b3
Xiaowen <liuxiaowencs@gmail.com>
2015-01-08 15:25:43 -0500

    Added zip-linux.sh

---
+++ releases/zip-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ "$#" -ne 1 ];then
+  echo "Usage: $0 version_number" >&2
+  exit 1
+fi
+
+mkdir toppic-linux-${1}
+cp ../bin/toppic toppic-linux-${1} 
+cp ../LICENSE toppic-linux-${1}
+cp -r ../toppic_resources toppic-linux-${1}
+
+zip -r toppic-linux-${1}.zip toppic-linux-${1}
+
--- zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ "$#" -ne 1 ];then
-  echo "Usage: $0 version_number" >&2
-  exit 1
-fi
-
-cp ./bin/toppic .
-zip -r toppic-$1-linux.zip ./toppic_resources/* toppic LICENSE
-
