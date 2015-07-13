65a0027ba9b6ef384dd07e9e013b352061a9923c
Xiaowen <liuxiaowencs@gmail.com>
2015-07-13 16:58:52 -0400

    Update zip-win.bat

--- releases/zip-win.bat
+++ releases/zip-win.bat
@@ -5,5 +5,3 @@ cp -r ..\toppic_resources toppic-win-%1
 cp C:\mingw64\bin\libgcc_s_seh-1.dll toppic-win-%1
 cp C:\mingw64\bin\libstdc++-6.dll toppic-win-%1
 cp C:\mingw64\bin\libwinpthread-1.dll toppic-win-%1
-
-
