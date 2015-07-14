7086d8199b7b56ab6ba751e45f1a5916869a3bb0
unknown <xwliu@in-info-wk304xl.ads.iu.edu>
2015-07-14 16:27:02 -0400

    UPdated zip-win.bat and toppic.iss

--- releases/zip-win.bat
+++ releases/zip-win.bat
@@ -2,6 +2,7 @@ mkdir toppic-win-%1
 cp ..\bin\toppic.exe toppic-win-%1 
 cp ..\LICENSE toppic-win-%1
 cp -r ..\toppic_resources toppic-win-%1
+cp .\inno_setup_script\toppic.ico toppic-win-%1
 cp C:\mingw64\bin\libgcc_s_seh-1.dll toppic-win-%1
 cp C:\mingw64\bin\libstdc++-6.dll toppic-win-%1
 cp C:\mingw64\bin\libwinpthread-1.dll toppic-win-%1
