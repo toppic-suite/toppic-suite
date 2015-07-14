3888d8700f3bf4389a54c7ae8d97ce0dec030a4a
Xiaowen <liuxiaowencs@gmail.com>
2015-07-14 15:23:03 -0400

    Added console summary

---
+++ src/console/summary.cpp
@@ -0,0 +1,29 @@
+#include "base/file_util.hpp"
+#include "console/summary.hpp"
+
+namespace prot {
+
+void Summary::outputSummary(Argument arguments, 
+                            boost::posix_time::ptime start_time,
+                            boost::posix_time::ptime stop_time) {
+  
+  std::string spec_file_name = arguments.getArguments()["spectrumFileName"];
+  std::string base_name = basename(spec_file_name);
+  std::string output_file_name = base_name + "." + "SUMMARY";
+
+  std::ofstream output; 
+  output.open(output_file_name, std::ios::out | std::ios::app);
+
+  boost::posix_time::time_duration duration;
+  duration = stop_time - start_time;
+  output << "1. Time" << std::endl;
+  output << "Start time: " << boost::posix_time::to_simple_string(start_time) << std::endl;
+  output << "Stop time: " << boost::posix_time::to_simple_string(stop_time) << std::endl;
+  output << "Running time: " << boost::posix_time::to_simple_string(duration) << std::endl;
+
+
+  arguments.outputArguments(output);
+  output.close();
+}
+
+} /* namespace prot */
