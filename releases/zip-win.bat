04a006425e8cda13a93c3bc47e64730e494ca904
Xiaowen <xwliu@iupui.edu>
2015-07-14 21:07:46 -0400

    Update zip-win.bat

--- releases/zip-win.bat
+++ releases/zip-win.bat
@@ -1,8 +1,8 @@
 mkdir toppic-win-%1
-cp ..\bin\toppic.exe toppic-win-%1 
-cp ..\LICENSE toppic-win-%1
-cp -r ..\toppic_resources toppic-win-%1
-cp .\inno_setup_script\toppic.ico toppic-win-%1
-cp C:\mingw64\bin\libgcc_s_seh-1.dll toppic-win-%1
-cp C:\mingw64\bin\libstdc++-6.dll toppic-win-%1
-cp C:\mingw64\bin\libwinpthread-1.dll toppic-win-%1
+copy ..\bin\toppic.exe toppic-win-%1 
+copy ..\LICENSE toppic-win-%1
+xcopy /S ..\toppic_resources toppic-win-%1\toppic_resources
+copy .\inno_setup_script\toppic.ico toppic-win-%1
+copy C:\mingw64\bin\libgcc_s_seh-1.dll toppic-win-%1
+copy "C:\mingw64\bin\libstdc++-6.dll" toppic-win-%1
+copy C:\mingw64\bin\libwinpthread-1.dll toppic-win-%1
