62bb2262475696353931860e4c22e2703d3da61d
Xiaowen <liuxiaowencs@gmail.com>
2015-11-10 18:04:10 -0500

    Working on proteoform factory

--- src/base/proteoform_reader.hpp
+++ src/base/proteoform_reader.hpp
@@ -1,5 +1,5 @@
-#ifndef PROT_PROTEOFORM_READER_HPP_
-#define PROT_PROTEOFORM_READER_HPP_
+#ifndef PROT_BASE_PROTEOFORM_READER_HPP_
+#define PROT_BASE_PROTEOFORM_READER_HPP_
 
 #include <iostream>
 #include <fstream>
@@ -12,7 +12,6 @@
 #undef BOOST_NO_CXX11_SCOPED_ENUMS
 
 #include "htslib/faidx.h"
-
 
 #include "base/string_util.hpp"
 #include "base/residue_seq.hpp"
@@ -32,25 +31,18 @@ class ProteoformReader {
 
   void setSeqId (int id) {seq_id_ = id;}
 
+  static ProteoformPtrVec readFastaToProteoform(const std::string &file_name, 
+                                                const ResiduePtrVec &residue_list);
+
+
+  static ProteoformPtrVec readFastaToProteoform(const std::string &file_name, 
+                                                const ResiduePtrVec &residue_list,
+                                                int seq_bgn_id);
+
  private:
   FastaReaderPtr reader_ptr_;
   int seq_id_ = 0;
 };
 
-ProteoformPtrVec readFastaToProteoform(const std::string &file_name, 
-                                       const ResiduePtrVec &residue_list);
-                                       
-
-ProteoformPtrVec readFastaToProteoform(const std::string &file_name, 
-                                       const ResiduePtrVec &residue_list,
-                                       int seq_bgn_id);
-
-ProteoformPtr readFastaToProteoform(faidx_t *fai,
-                                    int id,
-                                    const std::string &seq_name, 
-                                    const std::string &seq_desc,
-                                    const ResiduePtrVec &residue_list);
-
 }
-
 #endif
