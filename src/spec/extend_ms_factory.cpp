100efd563f072657c0a45fd6943fbd30a072d3b7
Xiaowen Liu <xwliu@iupui.edu>
2015-11-11 22:27:19 -0500

    Complete extend_ms_factory

--- src/spec/extend_ms_factory.cpp
+++ src/spec/extend_ms_factory.cpp
@@ -1,40 +1,12 @@
-#include "spec/extend_peak.hpp"
+#include "spec/extend_ms_factory.hpp"
 
 namespace prot {
 
-ExtendPeak::ExtendPeak(DeconvPeakPtr base_peak_ptr, 
-                       double mono_mass,double score)
-    :Peak(mono_mass,1.0){
-  base_peak_ptr_ = base_peak_ptr;
-  mono_mass_ = mono_mass;
-  score_ = score;
-  orig_tolerance_ = 0.0;
-  reverse_tolerance_ = 0.0;
-}
-
-void ExtendPeak::appendXml(XmlDOMDocument* xml_doc,
-                           xercesc::DOMElement* parent) {
-  xercesc::DOMElement* element = xml_doc->createElement("extend_peak");
-  std::string str = convertToString(getPosition());
-  xml_doc->addElement(element, "position", str.c_str());
-  str = convertToString(getIntensity());
-  xml_doc->addElement(element, "intensity", str.c_str());
-  str = convertToString(mono_mass_);
-  xml_doc->addElement(element, "mono_mass", str.c_str());
-  str = convertToString(score_);
-  xml_doc->addElement(element, "score", str.c_str());
-  str = convertToString(orig_tolerance_);
-  xml_doc->addElement(element, "orig_tolerance", str.c_str());
-  str = convertToString(reverse_tolerance_);
-  xml_doc->addElement(element, "reverse_tolerance_", str.c_str());
-  base_peak_ptr_->appendXml(xml_doc,element);
-  parent->appendChild(element);
-}
-
-
-ExtendMsPtr createMsThreePtr(DeconvMsPtr deconv_ms_ptr, SpParaPtr sp_para_ptr, 
-                             double new_prec_mass) {
-  MsHeaderPtr header_ptr = getHeaderPtr(deconv_ms_ptr, new_prec_mass);
+ExtendMsPtr ExtendMsFactory::geneMsThreePtr(
+    DeconvMsPtr deconv_ms_ptr, SpParaPtr sp_para_ptr, 
+    double new_prec_mass) {
+  MsHeaderPtr ori_header_ptr = deconv_ms_ptr->getMsHeaderPtr();
+  MsHeaderPtr header_ptr = MsHeader::geneMsHeaderPtr(ori_header_ptr, new_prec_mass);
 
   ExtendPeakPtrVec list;
   double ext_min_mass = sp_para_ptr->getExtendMinMass();
@@ -69,7 +41,7 @@ ExtendMsPtr createMsThreePtr(DeconvMsPtr deconv_ms_ptr, SpParaPtr sp_para_ptr,
   }
 
   // sort 
-  std::sort(list_filtered.begin(),list_filtered.end(),extendPeakUp);
+  std::sort(list_filtered.begin(),list_filtered.end(),ExtendPeak::cmpPosIncrease);
 
   //set error tolerance
   PeakTolerancePtr peak_tole_ptr = sp_para_ptr->getPeakTolerancePtr();
@@ -85,67 +57,12 @@ ExtendMsPtr createMsThreePtr(DeconvMsPtr deconv_ms_ptr, SpParaPtr sp_para_ptr,
   return ExtendMsPtr(new Ms<ExtendPeakPtr>(header_ptr,list_filtered, ppo));
 }
 
-inline bool massErrorUp(const std::pair<int, int> &a, const std::pair<int,int> b) {
-    return a.first < b.first;
-}
-
-std::vector<std::pair<int, int>> getExtendIntMassErrorList(const ExtendMsPtrVec &ext_ms_ptr_vec, 
-        double scale){
-    std::vector<std::pair<int,int>> mass_errors;
-    for (size_t i = 0; i < ext_ms_ptr_vec.size(); i++) {
-        ExtendMsPtr ext_ms_ptr = ext_ms_ptr_vec[i];
-        std::pair<int,int> last_mass_error(-1, 0);
-        for(size_t j=0; j<ext_ms_ptr->size(); j++){
-            int m = (int) std::round(ext_ms_ptr->getPeakPtr(j)->getPosition()*scale);
-            int e = (int) std::ceil(ext_ms_ptr->getPeakPtr(j)->getOrigTolerance()*scale);
-            std::pair<int,int> cur_m_e (m, e);
-            if(cur_m_e.first != last_mass_error.first){
-                mass_errors.push_back(cur_m_e);
-                last_mass_error = cur_m_e;
-            }
-            else if(cur_m_e.second > last_mass_error.second){
-                mass_errors.pop_back();
-                mass_errors.push_back(cur_m_e);
-                last_mass_error = cur_m_e;
-            }
-        }
-    }
-
-    std::sort(mass_errors.begin(), mass_errors.end(),massErrorUp);
-    return mass_errors;
-}
-
-/*std::pair<std::vector<int>, std::vector<int>> getExtendIntMassErrorList(*/
-    //ExtendMsPtr ext_ms_ptr, double scale) {
-  //std::vector<int> masses;
-  //std::vector<int> errors;
-  //int last_mass = -1;
-  //int last_error = 0;
-  //for(size_t i=0;i<ext_ms_ptr->size();i++){
-    //int m = (int) std::round(ext_ms_ptr->getPeakPtr(i)->getPosition()*scale);
-    //int e = (int) std::ceil(ext_ms_ptr->getPeakPtr(i)->getOrigTolerance()*scale);
-    //if(m != last_mass){
-      //masses.push_back(m);
-      //errors.push_back(e);
-      //last_mass = m;
-      //last_error = e;
-    //}
-    //else if(e>last_error){
-      //errors.pop_back();
-      //errors.push_back(e);
-      //last_error = e;
-    //}
-  //}
-  //std::pair<std::vector<int>, std::vector<int>> results( masses, errors);
-  //return results;
-/*}*/
-
-ExtendMsPtrVec createMsThreePtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec, 
-                                   SpParaPtr sp_para_ptr, double new_prec_mass) {
+ExtendMsPtrVec ExtendMsFactory::geneMsThreePtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec, 
+                                                  SpParaPtr sp_para_ptr, double new_prec_mass) {
   ExtendMsPtrVec extend_ms_ptr_vec;
   for (size_t i = 0; i < deconv_ms_ptr_vec.size(); i++) {
     extend_ms_ptr_vec.push_back(
-        createMsThreePtr(deconv_ms_ptr_vec[i], sp_para_ptr, new_prec_mass));
+        geneMsThreePtr(deconv_ms_ptr_vec[i], sp_para_ptr, new_prec_mass));
   }
   return extend_ms_ptr_vec;
 }
--- src/spec/extend_ms_factory.hpp
+++ src/spec/extend_ms_factory.hpp
@@ -1,82 +1,21 @@
-#ifndef PROT_EXTEND_PEAK_HPP_
-#define PROT_EXTEND_PEAK_HPP_
+#ifndef PROT_SPEC_EXTEND_MS_FACTORY_HPP_
+#define PROT_SPEC_EXTEND_MS_FACTORY_HPP_
 
-#include <vector>
-#include <memory>
-#include <algorithm>
-
-#include "spec/deconv_peak.hpp"
-#include "spec/ms.hpp"
 #include "spec/deconv_ms.hpp"
+#include "spec/extend_ms.hpp"
 #include "spec/sp_para.hpp"
 
 namespace prot {
 
-class ExtendPeak : public Peak{
- public:
-  ExtendPeak();
+class ExtendMsFactory {
 
-  ExtendPeak(DeconvPeakPtr base_peak_ptr, double mono_mass, double score);
+  static ExtendMsPtr geneMsThreePtr(DeconvMsPtr deconv_ms_ptr, SpParaPtr sp_para_ptr,
+                                    double new_prec_mass);
 
-  DeconvPeakPtr getBasePeakPtr(){return base_peak_ptr_;}
-
-  double getMonoMass(){return mono_mass_;}
-
-  double getScore(){return score_;}
-
-  double getOrigTolerance(){return orig_tolerance_;}
-
-  double getReverseTolerance(){return reverse_tolerance_;}
-
-  void setOrigTolerance(double orig_tolerance) {
-    orig_tolerance_ = orig_tolerance;}
-
-  void setReverseTolerance(double reverse_tolerance) {
-    reverse_tolerance_ = reverse_tolerance;}
-
-  void appendXml(XmlDOMDocument* xml_doc,xercesc::DOMElement* parent);
-
- private:
-  DeconvPeakPtr base_peak_ptr_;
-  double mono_mass_;
-  double score_;
-  double orig_tolerance_;
-  double reverse_tolerance_;
+  static ExtendMsPtrVec geneMsThreePtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec, 
+                                          SpParaPtr sp_para_ptr, double new_prec_mass);
 };
-
-typedef std::shared_ptr<ExtendPeak> ExtendPeakPtr;
-typedef std::vector<ExtendPeakPtr> ExtendPeakPtrVec;
-typedef std::shared_ptr<Ms<ExtendPeakPtr>> ExtendMsPtr;
-typedef std::vector<ExtendMsPtr> ExtendMsPtrVec;
-
-ExtendMsPtr createMsThreePtr(DeconvMsPtr deconv_ms_ptr, SpParaPtr sp_para_ptr,
-                             double new_prec_mass);
-
-ExtendMsPtrVec createMsThreePtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec, 
-                                   SpParaPtr sp_para_ptr, double new_prec_mass);
-
-/*std::pair<std::vector<int>, std::vector<int>> getExtendIntMassErrorList(*/
-    /*ExtendMsPtr ext_ms_ptr, double scale);*/
-
-std::vector<std::pair<int, int>> 
-getExtendIntMassErrorList(const ExtendMsPtrVec &ext_ms_ptr_vec, double scale); 
-
-inline bool extendPeakUp(const ExtendPeakPtr &a, const ExtendPeakPtr &b){
-  return a->getPosition() < b->getPosition();
-}
-
-
-/* use inline to speedup */
-inline std::vector<double> getExtendMassVec (ExtendMsPtr extend_ms_ptr) {
-  std::vector<double> masses;
-  ExtendPeakPtrVec peak_ptr_list = extend_ms_ptr->getPeakPtrVec();
-  for (size_t i = 0; i < peak_ptr_list.size(); i++) {
-    masses.push_back(peak_ptr_list[i]->getPosition());
-  }
-  return masses;
-}
-
 
 } /* namespace prot */
 
-#endif /* EXTEND_PEAK_HPP_ */
+#endif 
