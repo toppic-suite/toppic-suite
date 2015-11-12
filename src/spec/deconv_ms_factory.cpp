3b8196d6fc82fb8c5b4747f89aca7860adf92ca4
Xiaowen Liu <xwliu@iupui.edu>
2015-11-11 22:07:38 -0500

    Complete deconv_ms_factory

--- src/spec/deconv_ms_factory.cpp
+++ src/spec/deconv_ms_factory.cpp
@@ -1,19 +1,19 @@
-
 #include <memory>
 
 #include "spec/peak.hpp"
 #include "spec/ms.hpp"
-#include "spec/deconv_ms.hpp"
+#include "spec/deconv_ms_factory.hpp"
 
 namespace prot {
 
 
-DeconvMsPtrVec getRefineMsPtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec, 
-                                 double new_prec_mass) {
+DeconvMsPtrVec DeconvMsFactory::getRefineMsPtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec, 
+                                                  double new_prec_mass) {
   DeconvMsPtrVec result_ptrs;
   for (size_t m = 0; m < deconv_ms_ptr_vec.size(); m++) {
     DeconvMsPtr deconv_ms_ptr = deconv_ms_ptr_vec[m];
-    MsHeaderPtr header_ptr = getHeaderPtr(deconv_ms_ptr, new_prec_mass);
+    MsHeaderPtr ori_header_ptr = deconv_ms_ptr->getMsHeaderPtr();
+    MsHeaderPtr header_ptr = MsHeader::geneMsHeaderPtr(ori_header_ptr, new_prec_mass);
     std::vector<DeconvPeakPtr> peak_ptr_list;
     for (size_t p = 0; p < deconv_ms_ptr->size(); p++) {
       DeconvPeakPtr ori_peak_ptr = deconv_ms_ptr->getPeakPtr(p);
@@ -28,12 +28,4 @@ DeconvMsPtrVec getRefineMsPtrVec(const DeconvMsPtrVec &deconv_ms_ptr_vec,
   return result_ptrs;
 }
 
-MsHeaderPtr getHeaderPtr(DeconvMsPtr deconv_ms_ptr, double new_prec_mass) {
-  MsHeaderPtr header_ptr = deconv_ms_ptr->getHeaderPtr();
-  MsHeaderPtr new_header_ptr(new MsHeader(*header_ptr.get()));
-  double mono_mz = compMonoMz(new_prec_mass, header_ptr->getPrecCharge());
-  new_header_ptr->setPrecMonoMz(mono_mz);
-  return new_header_ptr;
-}
-    
 }
