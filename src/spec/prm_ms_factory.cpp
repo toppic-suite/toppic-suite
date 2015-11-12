b9a6883be1161b2f1985e2825624d4c37e8464d6
Xiaowen Liu <xwliu@iupui.edu>
2015-11-11 23:16:30 -0500

    Working on prm_ms

--- src/spec/prm_ms_factory.cpp
+++ src/spec/prm_ms_factory.cpp
@@ -103,30 +103,6 @@ void filterPeaks(const PrmPeakPtrVec &peak_list, PrmPeakPtrVec &filtered_list,
   }
 }
 
-PrmPeakPtr getZeroPeakPtr(int spec_id, double prec_mono_mass, PeakTolerancePtr tole_ptr, double score) {
-  //zero_peak
-  DeconvPeakPtr zero_peak_ptr = DeconvPeakPtr(new DeconvPeak(-1,0,0,0));
-  PrmPeakPtr prm_peak_ptr(new PrmPeak(spec_id, zero_peak_ptr,PRM_PEAK_TYPE_ORIGINAL,0, score));
-  // set tolerance 
-  prm_peak_ptr->setStrictTolerance(tole_ptr->compStrictErrorTole(0));
-  prm_peak_ptr->setNStrictCRelacTolerance(tole_ptr->compStrictErrorTole(0));
-  prm_peak_ptr->setNRelaxCStrictTolerance(tole_ptr->compStrictErrorTole(prec_mono_mass));
-  return prm_peak_ptr;
-}
-
-PrmPeakPtr getPrecPeakPtr(int spec_id, double prec_mono_mass, PeakTolerancePtr tole_ptr, double score) {
-  //prec_peak
-  double prec_peak_shift = IonTypeFactory::getIonTypePtr_PREC()->getShift();
-  double prec_peak_mass = prec_mono_mass - prec_peak_shift;
-  DeconvPeakPtr prec_peak_ptr(new DeconvPeak(-1,prec_peak_mass, 0,0));
-  PrmPeakPtr prm_peak_ptr = PrmPeakPtr(
-      new PrmPeak(spec_id,prec_peak_ptr,PRM_PEAK_TYPE_ORIGINAL,prec_peak_mass, score));
-  // set tolerance
-  prm_peak_ptr->setStrictTolerance(tole_ptr->compStrictErrorTole(prec_mono_mass));
-  prm_peak_ptr->setNStrictCRelacTolerance(tole_ptr->compStrictErrorTole(prec_mono_mass));
-  prm_peak_ptr->setNRelaxCStrictTolerance(tole_ptr->compStrictErrorTole(0));
-  return prm_peak_ptr;
-}
 
 PrmMsPtr createMsTwoPtr(DeconvMsPtr deconv_ms_ptr, int spec_id, SpParaPtr sp_para_ptr,
                         double prec_mono_mass){
