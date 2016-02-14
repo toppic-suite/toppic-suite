72db6fcdf4e17e6a78595a73b6a6993c82d7d46f
Qiang Kou <qkou@umail.iu.edu>
2016-02-13 22:57:50 -0500

    update releases/zip-linux.sh

--- releases/zip-linux.sh
+++ releases/zip-linux.sh
@@ -7,6 +7,8 @@ fi
 mkdir toppic-linux-${1}
 cp ../bin/toppic toppic-linux-${1} 
 cp ../LICENSE toppic-linux-${1}
+cp ../var_mods.txt toppic-linux-${1}
+cp ../fix_mods.txt toppic-linux-${1}
 cp -r ../toppic_resources toppic-linux-${1}
 
 zip -r toppic-linux-${1}.zip toppic-linux-${1}
