c42f47481de940a6f80939278f8e4c9299f5ecbf
Qiang Kou <qkou@umail.iu.edu>
2016-02-16 11:40:21 -0500

    compiled the mass graph part

--- src/test/test_graph_align.cpp
+++ src/test/test_graph_align.cpp
@@ -1,40 +1,20 @@
 #include "base/fasta_reader.hpp"
+#include "base/fasta_util.hpp"
 #include "base/base_data.hpp"
-#include "base/web_logger.hpp"
 
 #include "spec/msalign_reader.hpp"
+#include "spec/msalign_util.hpp"
 
-#include "prsm/prsm_para.hpp"
-#include "prsm/prsm_combine.hpp"
-#include "prsm/prsm_str_combine.hpp"
-#include "prsm/prsm_selector.hpp"
-#include "prsm/output_selector.hpp"
-#include "prsm/prsm_species.hpp"
-#include "prsm/simple_prsm_writer.hpp"
-#include "prsm/simple_prsm_str_combine.hpp"
-#include "prsm/table_writer.hpp"
-#include "prsm/prsm_fdr.hpp"
-
-#include "zeroptmsearch/zero_ptm_mng.hpp"
-#include "zeroptmsearch/zero_ptm_search.hpp"
-
-#include "diagfilter/diag_filter_mng.hpp"
-#include "diagfilter/diag_filter_processor.hpp"
-
-#include "oneptmfilter/one_ptm_filter_mng.hpp"
-#include "oneptmfilter/one_ptm_filter_processor.hpp"
-
-#include "ptmsearch/ptm_mng.hpp"
-#include "ptmsearch/ptm_processor.hpp"
-
-#include "poisson/poisson_processor.hpp"
-#include "poisson/poisson_mng.hpp"
 
 #include "tdgf/evalue_processor.hpp"
 #include "tdgf/tdgf_mng.hpp"
 
-#include "prsmview/xml_generator.hpp"
-#include "prsmview/transformer.hpp"
+#include "prsm/prsm_para.hpp"
+#include "prsm/prsm_str_combine.hpp"
+#include "prsm/prsm_top_selector.hpp"
+#include "prsm/prsm_cutoff_selector.hpp"
+#include "prsm/prsm_species.hpp"
+#include "prsm/prsm_table_writer.hpp"
 
 #include "console/argument.hpp"
 #include "console/summary.hpp"
@@ -61,7 +41,7 @@ int proteoform_graph_test(int argc, char* argv[]) {
 
     std::string exe_dir = arguments["executiveDir"];
     std::cout << "Executive file directory is: " << exe_dir << std::endl;
-    initBaseData(exe_dir);
+    BaseData::init(exe_dir);
 
     LOG_DEBUG("Init base data completed");
 
@@ -74,13 +54,11 @@ int proteoform_graph_test(int argc, char* argv[]) {
     int ptm_num = std::stoi(arguments["ptmNumber"]);
 
     /*
-    int n_top = std::stoi(arguments["numOfTopPrsms"]);
-    double max_ptm_mass = std::stod(arguments["maxPtmMass"]);
-    bool use_gf = false; 
-    if (arguments["useGf"] == "true") {
-      use_gf = true;
-    }
-    */
+       bool use_gf = false; 
+       if (arguments["useGf"] == "true") {
+       use_gf = true;
+       }
+       */
 
     bool decoy = false;
     if (arguments["searchType"] == "TARGET+DECOY") {
@@ -90,13 +68,13 @@ int proteoform_graph_test(int argc, char* argv[]) {
     LOG_DEBUG("block size " << arguments["databaseBlockSize"]);
     int db_block_size = std::stoi(arguments["databaseBlockSize"]);
 
-    dbPreprocess (ori_db_file_name, db_file_name, decoy, db_block_size);
-    generateSpIndex(sp_file_name);
+    FastaUtil::dbPreprocess(ori_db_file_name, db_file_name, decoy, db_block_size);
+    MsAlignUtil::geneSpIndex(sp_file_name);
 
     PrsmParaPtr prsm_para_ptr = PrsmParaPtr(new PrsmPara(arguments));
 
     std::cout << "Graph alignment started." << std::endl;
-    
+
     int max_mod_num = 10;
     GraphAlignMngPtr ga_mng_ptr = GraphAlignMngPtr(new GraphAlignMng(prsm_para_ptr, residue_mod_file_name, ptm_num, max_mod_num,  "GRAPH_ALIGN"));
     LOG_DEBUG("shift num " << ptm_num);
@@ -115,52 +93,53 @@ int proteoform_graph_test(int argc, char* argv[]) {
     std::cout << "Combining PRSMs finished." << std::endl;
 
     /*
-    std::cout << "E-value computation started." << std::endl;
-    bool variable_ptm = true;
-    TdgfMngPtr tdgf_mng_ptr = TdgfMngPtr(new TdgfMng (prsm_para_ptr, ptm_num, max_ptm_mass, use_gf,
-                                                      variable_ptm, "GRAPH_ALIGN", "EVALUE"));
-    EValueProcessorPtr processor = EValueProcessorPtr(new EValueProcessor(tdgf_mng_ptr));
-    processor->init();
-    LOG_DEBUG("e value init complete");
+       std::cout << "E-value computation started." << std::endl;
+       bool variable_ptm = true;
+       TdgfMngPtr tdgf_mng_ptr = TdgfMngPtr(new TdgfMng (prsm_para_ptr, ptm_num, max_ptm_mass, use_gf,
+       variable_ptm, "GRAPH_ALIGN", "EVALUE"));
+       EValueProcessorPtr processor = EValueProcessorPtr(new EValueProcessor(tdgf_mng_ptr));
+       processor->init();
+       LOG_DEBUG("e value init complete");
     // compute E-value for a set of prsms each run 
     processor->process(false);
     processor = nullptr;
 
     if (arguments["searchType"]=="TARGET") { 
-      std::cout << "Top PRSM selecting started" << std::endl;
-      PrsmSelectorPtr selector = PrsmSelectorPtr(new PrsmSelector(db_file_name, sp_file_name, "EVALUE", "TOP", n_top));
-      selector->process();
-      selector = nullptr;
-      std::cout << "Top PRSM selecting finished." << std::endl;
+    std::cout << "Top PRSM selecting started" << std::endl;
+    PrsmSelectorPtr selector = PrsmSelectorPtr(new PrsmSelector(db_file_name, sp_file_name, "EVALUE", "TOP", n_top));
+    selector->process();
+    selector = nullptr;
+    std::cout << "Top PRSM selecting finished." << std::endl;
     }
     else {
-      std::cout << "Top PRSM selecting started " << std::endl;
-      PrsmSelectorPtr selector = PrsmSelectorPtr(new PrsmSelector(db_file_name, sp_file_name, "EVALUE", "TOP_PRE", n_top));
-      selector->process();
-      selector = nullptr;
-      std::cout << "Top PRSM selecting finished." << std::endl;
+    std::cout << "Top PRSM selecting started " << std::endl;
+    PrsmSelectorPtr selector = PrsmSelectorPtr(new PrsmSelector(db_file_name, sp_file_name, "EVALUE", "TOP_PRE", n_top));
+    selector->process();
+    selector = nullptr;
+    std::cout << "Top PRSM selecting finished." << std::endl;
 
-      std::cout << "FDR computation started. " << std::endl;
-      PrsmFdrPtr fdr = PrsmFdrPtr(new PrsmFdr(db_file_name, sp_file_name, "TOP_PRE", "TOP"));
-      fdr->process();
-      fdr = nullptr;
-      std::cout << "FDR computation finished." << std::endl;
+    std::cout << "FDR computation started. " << std::endl;
+    PrsmFdrPtr fdr = PrsmFdrPtr(new PrsmFdr(db_file_name, sp_file_name, "TOP_PRE", "TOP"));
+    fdr->process();
+    fdr = nullptr;
+    std::cout << "FDR computation finished." << std::endl;
     }
-    
+
     std::cout << "PRSM selecting by cutoff started." << std::endl;
     std::string cutoff_type = arguments["cutoffType"];
     double cutoff_value;
     std::istringstream (arguments["cutoffValue"]) >> cutoff_value;
     OutputSelectorPtr output_selector = OutputSelectorPtr(
-        new OutputSelector(db_file_name, sp_file_name, "TOP", "CUTOFF_RESULT", 
-                           cutoff_type, cutoff_value));
+    new OutputSelector(db_file_name, sp_file_name, "TOP", "CUTOFF_RESULT", 
+    cutoff_type, cutoff_value));
     output_selector->process();
     output_selector = nullptr;
     std::cout << "PRSM selecting by cutoff finished." << std::endl;
     */
 
-    std::cout << "Outputting table started." << std::endl;
-    TableWriterPtr table_out = TableWriterPtr(new TableWriter(prsm_para_ptr, "RAW_RESULT", "OUTPUT_TABLE"));
+    std::cout << "Outputting table starts " << std::endl;
+    PrsmTableWriterPtr table_out = PrsmTableWriterPtr(
+        new PrsmTableWriter(prsm_para_ptr, "RAW_RESULT", "OUTPUT_TABLE"));
     table_out->write();
     table_out = nullptr;
     std::cout << "Outputting table finished." << std::endl;
