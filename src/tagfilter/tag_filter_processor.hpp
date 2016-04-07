55cac40f54450d08ce7003fd7a1598d2b92a7d47
Qiang Kou <qkou@umail.iu.edu>
2016-04-07 15:26:16 -0400

    update

---
+++ src/tagfilter/tag_filter_processor.hpp
@@ -0,0 +1,25 @@
+
+#ifndef PROT_TAG_FILTER
+#define PROT_TAG_FILTER
+
+#include "base/db_block.hpp"
+#include "tagfilter/tag_filter_mng.hpp"
+
+namespace prot {
+
+class TagFilterProcessor {
+ public:
+  TagFilterProcessor(TagFilterMngPtr mng_ptr);
+  void process();
+
+ private:
+  TagFilterMngPtr mng_ptr_;
+  void processBlock(DbBlockPtr block_ptr, int total_block_num);
+
+};
+
+typedef std::shared_ptr<TagFilterProcessor> TagFilterProcessorPtr;
+
+}
+
+#endif
