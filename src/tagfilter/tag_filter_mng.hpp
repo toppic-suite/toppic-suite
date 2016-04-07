6be6f66ba66fbbe3f73b4a890d63e92321c27b38
Qiang Kou <qkou@umail.iu.edu>
2016-04-07 15:18:43 -0400

    tagfinder -> tagfilter

---
+++ src/tagfilter/tag_filter_mng.hpp
@@ -0,0 +1,29 @@
+
+#ifndef PROT_TAG_FILTER_MNG
+#define PROT_TAG_FILTER_MNG
+
+#include <string>
+
+#include "prsm/prsm_para.hpp"
+
+namespace prot {
+
+class TagFilterMng {
+ public:
+  TagFilterMng(PrsmParaPtr prsm_para_ptr, int filtering_result_num,
+               const std::string &output_file_ext) {
+    prsm_para_ptr_ = prsm_para_ptr;
+    filter_result_num_ = filtering_result_num;
+    output_file_ext_ = output_file_ext;
+  }
+
+  PrsmParaPtr prsm_para_ptr_;
+  int filter_result_num_ = 20;
+  std::string output_file_ext_;
+
+};
+
+typedef std::shared_ptr<TagFilterMng> TagFilterMngPtr;
+
+}
+#endif
---
+++ src/test/test_tag_filter.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <iomanip>
+
+#include "base/fasta_reader.hpp"
+#include "base/fasta_util.hpp"
+#include "base/base_data.hpp"
+
+#include "spec/msalign_reader.hpp"
+#include "spec/msalign_util.hpp"
+#include "prsm/prsm_para.hpp"
+
+#include "tagfilter/tag_filter_mng.hpp"
+#include "tagfilter/tag_filter.hpp"
+
+#include "console/argument.hpp"
+
+namespace prot {
+
+int two_base_opt(int argc, char* argv[]) {
+  try {
+    Argument argu_processor;
+    bool success = argu_processor.parse(argc, argv);
+    if (!success) {
+      return 1;
+    }
+    std::map<std::string, std::string> arguments = argu_processor.getArguments();
+    std::cout << "TopPIC 1.0" << std::endl;
+
+    std::string exe_dir = arguments["executiveDir"];
+    argu_processor.outputArguments(std::cout, arguments);
+
+    BaseData::init(exe_dir);
+
+    LOG_DEBUG("Init base data completed");
+
+    std::string db_file_name = arguments["databaseFileName"];
+    std::string sp_file_name = arguments["spectrumFileName"];
+    std::string ori_db_file_name = arguments["oriDatabaseFileName"];
+    std::string log_file_name = arguments["logFileName"];
+
+    int n_top = std::stoi(arguments["numOfTopPrsms"]);
+    int ptm_num = std::stoi(arguments["ptmNumber"]);
+    double max_ptm_mass = std::stod(arguments["maxPtmMass"]);
+    int filter_result_num = std::stoi(arguments["filteringResultNumber"]);
+
+    PrsmParaPtr prsm_para_ptr = PrsmParaPtr(new PrsmPara(arguments));
+    LOG_DEBUG("prsm para inited");
+
+    bool decoy = false;
+    if (arguments["searchType"] == "TARGET+DECOY") {
+      decoy = true;
+    }
+    LOG_DEBUG("block size " << arguments["databaseBlockSize"]);
+    int db_block_size = std::stoi(arguments["databaseBlockSize"]);
+
+    FastaUtil::dbPreprocess (ori_db_file_name, db_file_name, decoy, db_block_size);
+    MsAlignUtil::geneSpIndex(sp_file_name);
+    std::cout << "Tag filtering started." << std::endl;
+    TagFilterMngPtr diag_filter_mng_ptr 
+        = std::make_shared<TagFilterMng>(prsm_para_ptr, filter_result_num, "TAG_FILTER");
+
+    /*std::cout << "Tag searching started." << std::endl;*/
+    //TagfinderPtr tagfinder = std::make_shared<Tagfinder>(prsm_para_ptr, 3);
+    //tagfinder->process();
+    //tagfinder = nullptr;
+    /*std::cout << "Tag searching finished." << std::endl;*/
+  } catch (const char* e) {
+    std::cout << "[Exception]" << std::endl;
+    std::cout << e << std::endl;
+  }
+  std::cout << "TopPC finished." << std::endl;
+  return 0;
+}
+
+}
+
+int main(int argc, char* argv[]) {
+  //prot::log_level = 2;
+  std::cout << std::setprecision(10);
+  return prot::two_base_opt(argc, argv);
+}
