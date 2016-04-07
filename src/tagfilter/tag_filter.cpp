3ceb6d1b6256eb3ae110a954b4518c1a33b0cf21
Qiang Kou <qkou@umail.iu.edu>
2016-04-07 17:02:24 -0400

    update

--- src/tagfilter/tag_filter.cpp
+++ src/tagfilter/tag_filter.cpp
@@ -3,10 +3,18 @@
 
 namespace prot{
 
+seq_tag geneSeqTag(const std::string & seq) {
+
+
+}
+
 TagFilter::TagFilter(const ProteoformPtrVec &proteo_ptrs,
                      TagFilterMngPtr mng_ptr){
-
-
+  mng_ptr_ = mng_ptr;
+  for (size_t i = 0; i < proteo_ptrs.size(); i++) {
+    seq_tag_map_[proteo_ptrs[i]->getFastaSeqPtr()->getName()]
+        = geneSeqTag(proteo_ptrs[i]->getFastaSeqPtr()->getSeq());
+  }
 }
 
 SimplePrsmPtrVec TagFilter::getBestMatch(const PrmMsPtrVec &ms_ptr_vec) {
