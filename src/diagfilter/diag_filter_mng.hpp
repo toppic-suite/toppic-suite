cf05c1b1c6b0bfd112c4c79b5d0c594184a1f8ec
Qiang Kou <qkou@umail.iu.edu>
2016-05-22 22:49:37 -0400

    add thread num into mng

--- src/diagfilter/diag_filter_mng.hpp
+++ src/diagfilter/diag_filter_mng.hpp
@@ -6,26 +6,29 @@
 namespace prot {
 
 class DiagFilterMng {
-  public:
+ public:
 
-    DiagFilterMng(PrsmParaPtr prsm_para_ptr, int filtering_result_num,
-                  const std::string &output_file_ext) {
-        prsm_para_ptr_ = prsm_para_ptr;
-        filter_result_num_ = filtering_result_num;
-        output_file_ext_ = output_file_ext;
-    }
+  DiagFilterMng(PrsmParaPtr prsm_para_ptr, int filtering_result_num,
+                int thread_num,
+                const std::string &output_file_ext) {
+    prsm_para_ptr_ = prsm_para_ptr;
+    filter_result_num_ = filtering_result_num;
+    thread_num_ = thread_num;
+    output_file_ext_ = output_file_ext;
+  }
 
-    PrsmParaPtr prsm_para_ptr_;
+  PrsmParaPtr prsm_para_ptr_;
 
-    /** parameters for fast filteration */
-    int max_proteoform_mass_ = 20000;
+  /** parameters for fast filteration */
+  int max_proteoform_mass_ = 20000;
 
-    //Candidate protein number for each spectrum
-    size_t filter_result_num_ = 20;
-    int db_block_size_ = 5000000;
-    int filter_scale_ = 100;
+  //Candidate protein number for each spectrum
+  size_t filter_result_num_ = 20;
+  int db_block_size_ = 5000000;
+  int filter_scale_ = 100;
+  int thread_num_ = 1;
 
-    std::string output_file_ext_;
+  std::string output_file_ext_;
 
 };
 
