6d767d7a7cf859e24d968841fa916f747e36fcc7
Xiaowen <liuxiaowencs@gmail.com>
2016-07-06 15:49:47 -0400

    Add vertex_a

---
+++ src/feature/vertex_a.cpp
@@ -0,0 +1,24 @@
+#include "feature/vertex_a.hpp"
+
+namespace prot {
+
+VertexA::VertexA(FeatureMngPtr mng_ptr, int bgn_peak, 
+                 int pre_win_peak_num, int cur_win_peak_num):
+    Vertex(mng_ptr, bgn_peak, pre_win_peak_num, cur_win_peak_num) {
+      this_score_ = 0;
+      score_ = 0;
+      prev_vertex_ = -1;
+    }
+
+VertexA::VertexA(VertexAPtr ptr):
+    Vertex(ptr) {
+    }
+
+bool VertexA::addPreEnv(MatchEnvPtr env, int max_overlap) {
+  bool result = Vertex::addPreEnv(env, max_overlap);
+  this_score_ += env->getScore();
+  return result;
+}
+
+
+}
---
+++ src/feature/vertex_a.hpp
@@ -0,0 +1,44 @@
+#ifndef PROT_FEATURE_VERTEX_A_HPP_
+#define PROT_FEATURE_VERTEX_A_HPP_
+
+#include "feature/vertex.hpp"
+
+namespace prot {
+
+class VertexA;
+typedef std::shared_ptr<VertexA> VertexAPtr;
+
+class VertexA : public Vertex {
+ public:
+  VertexA(FeatureMngPtr mng_ptr, int bgn_peak, 
+          int pre_win_peak_num, int cur_win_peak_num);
+  VertexA(VertexAPtr ptr);
+
+  bool addPreEnv(MatchEnvPtr env, int max_overlap);
+
+  double getThisScr() {return this_score_;}
+
+  double getScrA() {return score_;}
+
+  void setScrA(double score) {score_ = score;}
+
+  int getPreA() {return prev_vertex_;}
+
+  void setPreA(int prev) {prev_vertex_ = prev;}
+
+ private:
+  // the sum of score_s of envelopes in previous window 
+  double this_score_;
+  // current score_ for dp, only the score_s of pre_match_env are included. The
+  // reason is that we can only determine the score_ for sharing model when
+  // envelopes in the next window are determined.
+  double score_;
+  // previous vertex for backtracking 
+  int prev_vertex_;
+};
+
+typedef std::shared_ptr<VertexA> VertexAPtr;
+typedef std::vector<VertexAPtr> VertexAPtrVec;
+
+}
+#endif
