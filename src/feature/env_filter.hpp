9a63d235d8c534fa7585d8b7879eb29310797bfe
Xiaowen <liuxiaowencs@gmail.com>
2016-07-06 11:53:24 -0400

    Added env_filter

---
+++ src/feature/env_filter.hpp
@@ -0,0 +1,20 @@
+#ifndef PROT_FEATURE_ENV_FILTER_HPP_
+#define PROT_FEATURE_ENV_FILTER_HPP_
+
+#include "feature/match_env.hpp"
+#include "feature/deconv_data.hpp"
+
+namespace prot {
+
+class EnvFilter {
+ public:
+  static void filter(MatchEnvPtr2D &match_envs, DeconvDataPtr data_ptr,
+                     FeatureMngPtr mng_ptr);
+  
+  static void multipleMassFilter(MatchEnvPtr2D &match_env, DeconvDataPtr data_ptr,
+                                 FeatureMngPtr mng_ptr);
+};
+
+}
+
+#endif
