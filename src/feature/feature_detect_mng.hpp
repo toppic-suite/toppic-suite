e6aff29f81d19f9a83cde92ee4e71d31b1c1bac3
Xiaowen <liuxiaowencs@gmail.com>
2016-07-22 16:20:57 -0400

    Working on feature_detect

--- src/feature/feature_detect_mng.hpp
+++ src/feature/feature_detect_mng.hpp
@@ -1,16 +1,25 @@
 #ifndef PROT_FEATURE_DETECT_MNG_HPP_
 #define PROT_FEATURE_DETECT_MNG_HPP_
 
+#include <vector>
+#include "spec/peak_tolerance.hpp"
+
 namespace prot {
 
 class FeatureDetectMng {
  public:
-  FeatureDetectMng() {};
+  FeatureDetectMng();
 
-  // error tolerance
-  double ppo_ = 0.000015;
+  std::vector<double> getExtMasses(double mass);
+
+  PeakTolerancePtr peak_tolerance_ptr_;
+
+  std::vector<double> ext_offsets_;
+
+  double extend_min_mass_ = 5000;
 
   int intv_width_ = 500;
+
 };
 
 typedef std::shared_ptr<FeatureDetectMng> FeatureDetectMngPtr;
