488cb9abfd6352cd6107c42b11dbc038cfb0f729
Xiaowen <liuxiaowencs@gmail.com>
2016-07-25 17:28:18 -0400

    :

--- src/feature/topfd.cpp
+++ src/feature/topfd.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 
+#include "base/file_util.hpp"
+#include "base/base_data.hpp"
 #include "feature/deconv_argu.hpp"
 #include "feature/deconv_para.hpp"
 #include "feature/deconv_process.hpp"
@@ -10,6 +12,8 @@ namespace prot {
 
 int deconvProcess(int argc, char* argv[]) {
   try {
+    std::string exe_dir = FileUtil::getExecutiveDir(argv[0]);
+    BaseData::init(exe_dir);
     /*
     DeconvArgument argu_processor;
     bool success = argu_processor.parse(argc, argv);
@@ -24,7 +28,9 @@ int deconvProcess(int argc, char* argv[]) {
     LOG_DEBUG("init process");
     process.process();
     */
+
     std::string file_name(argv[1]);
+    LOG_DEBUG("file name " << file_name);
     FeatureDetect::process(file_name);    
   } catch (const char* e) {
     std::cout << "[Exception]" << std::endl;
@@ -37,6 +43,6 @@ int deconvProcess(int argc, char* argv[]) {
 }
 
 int main(int argc, char* argv[]) {
-  prot::log_level = 2;
+//  prot::log_level = 2;
   return prot::deconvProcess(argc, argv);
 }
