57a84fcc21748fe31aab543e95fe44ef6309a11f
Qiang Kou <qkou@umail.iu.edu>
2017-01-12 17:22:48 -0500

    close #186

--- src/base/prot_mod_base.hpp
+++ src/base/prot_mod_base.hpp
@@ -32,6 +32,8 @@
 #ifndef PROT_BASE_PROT_MOD_BASE_HPP_
 #define PROT_BASE_PROT_MOD_BASE_HPP_
 
+#include <string>
+
 #include "base/ptm.hpp"
 #include "base/trunc.hpp"
 #include "base/prot_mod.hpp"
@@ -44,36 +46,31 @@ class ProtModBase {
  public:
   static void initBase(const std::string &file_name);
 
-  static const ProtModPtrVec& getBaseProtModPtrVec() {
-    return prot_mod_ptr_vec_;
-  }
+  static const ProtModPtrVec& getBaseProtModPtrVec() {return prot_mod_ptr_vec_;}
 
   static ProtModPtr getProtModPtrByName(const std::string &name);
 
   static ProtModPtrVec getProtModPtrByType(const std::string &type);
 
-  static ProtModPtr getProtModPtr_NONE () {
-    return prot_mod_ptr_NONE_;
-  }
+  static ProtModPtr getProtModPtr_NONE() {return prot_mod_ptr_NONE_;}
 
   static ProtModPtr getProtModPtrFromXml(xercesc::DOMElement * element);
 
   static std::string getType_NME() {return "NME";}
   static std::string getType_NME_ACETYLATION() {return "NME_ACETYLATION";}
+  static std::string getType_M_ACETYLATION() {return "M_ACETYLATION";}
 
-  static ProtModPtr getProtModPtr_M_ACETYLATION () {
-    return prot_mod_ptr_M_ACETYLATION_;
-  }
+  static ProtModPtr getProtModPtr_M_ACETYLATION() {return prot_mod_ptr_M_ACETYLATION_;}
 
   /*
-  static ProtModPtr getProtModPtr_NME () {
-    return prot_mod_ptr_NME_;
-  }
+     static ProtModPtr getProtModPtr_NME () {
+     return prot_mod_ptr_NME_;
+     }
 
-  static ProtModPtr getProtModPtr_NME_ACETYLATION () {
-    return prot_mod_ptr_NME_ACETYLATION_;
-  }
-  */
+     static ProtModPtr getProtModPtr_NME_ACETYLATION () {
+     return prot_mod_ptr_NME_ACETYLATION_;
+     }
+     */
 
  private:
   static ProtModPtrVec prot_mod_ptr_vec_;
