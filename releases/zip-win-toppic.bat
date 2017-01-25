f6f527a8c62cda131bf08f5acd4500a58d4b5540
Qiang Kou <qkou@umail.iu.edu>
2017-01-25 11:28:56 -0500

    update release script

--- releases/zip-win-toppic.bat
+++ releases/zip-win-toppic.bat
@@ -1,8 +1,14 @@
+rmdir /S toppic-win-%1
 mkdir toppic-win-%1
 copy ..\bin\toppic.exe toppic-win-%1 
 copy ..\LICENSE toppic-win-%1
 xcopy /S ..\toppic_resources toppic-win-%1\toppic_resources
-copy .\inno_setup_script\toppic.ico toppic-win-%1
+
 copy C:\mingw64\bin\libgcc_s_seh-1.dll toppic-win-%1
 copy "C:\mingw64\bin\libstdc++-6.dll" toppic-win-%1
 copy C:\mingw64\bin\libwinpthread-1.dll toppic-win-%1
+
+mkdir toppic-win-%1\example_files
+copy ..\testcases\data\mzxml_test.msalign toppic-win-%1\example_files
+copy ..\testcases\data\mzxml_test.fasta toppic-win-%1\example_files
+copy ..\localization_mods.txt toppic-win-%1\example_files
