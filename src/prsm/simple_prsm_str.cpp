a43532959709f942d1674a52d29dbaebc4b5d5da
Qiang Kou <qkou@umail.iu.edu>
2017-03-31 14:14:59 -0400

    close #229

--- src/prsm/simple_prsm_str.cpp
+++ src/prsm/simple_prsm_str.cpp
@@ -37,9 +37,9 @@ namespace prot {
 
 SimplePrsmStr::SimplePrsmStr(const std::vector<std::string> &str_vec) {
   str_vec_ = str_vec;
-  std::string line = PrsmUtil::getXmlLine(str_vec_,"spectrum_id"); 
+  std::string line = PrsmUtil::getXmlLine(str_vec_,"<spectrum_id>"); 
   spectrum_id_ = std::stoi(PrsmUtil::getValueStr(line));
-  line = PrsmUtil::getXmlLine(str_vec_,"score"); 
+  line = PrsmUtil::getXmlLine(str_vec_, "<score>"); 
   score_ = std::stod(PrsmUtil::getValueStr(line));
   //LOG_DEBUG("spectrum id " << spectrum_id_ << " score " << score_);
 }
