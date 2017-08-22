5621d4852f545fe337a9088e119b86bf5eddaf97
Qiang Kou <qkou@umail.iu.edu>
2017-08-22 11:34:00 -0400

    [LINT] coding style update (#246)

--- src/base/activation.cpp
+++ src/base/activation.cpp
@@ -28,6 +28,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <string>
 
 #include "base/activation.hpp"
 #include "base/ion_type_base.hpp"
@@ -36,24 +37,24 @@
 
 namespace prot {
 
-Activation::Activation(const std::string &name, 
-                       IonTypePtr n_ion_type_ptr, 
+Activation::Activation(const std::string &name,
+                       IonTypePtr n_ion_type_ptr,
                        IonTypePtr c_ion_type_ptr):
-    name_(name), 
+    name_(name),
     n_ion_type_ptr_(n_ion_type_ptr),
     c_ion_type_ptr_(c_ion_type_ptr) {
     }
 
 Activation::Activation(xercesc::DOMElement * element) {
   name_ = XmlDomUtil::getChildValue(element, "name", 0);
-  std::string ion_type_name = XmlDomUtil::getChildValue(element, "n_ion_type",0);
+  std::string ion_type_name = XmlDomUtil::getChildValue(element, "n_ion_type", 0);
   n_ion_type_ptr_ = IonTypeBase::getIonTypePtrByName(ion_type_name);
   ion_type_name = XmlDomUtil::getChildValue(element, "c_ion_type", 0);
   c_ion_type_ptr_ = IonTypeBase::getIonTypePtrByName(ion_type_name);
 }
 
 void Activation::appendNameToXml(XmlDOMDocument* xml_doc,
-                                 xercesc::DOMElement* parent){
+                                 xercesc::DOMElement* parent) {
   std::string element_name = Activation::getXmlElementName();
   xercesc::DOMElement* element = xml_doc->createElement(element_name.c_str());
   xml_doc->addElement(element, "name", name_.c_str());
@@ -65,4 +66,4 @@ std::string Activation::getNameFromXml(xercesc::DOMElement * element) {
   return name;
 }
 
-} 
+}  // namespace prot
--- src/base/algorithm.cpp
+++ src/base/algorithm.cpp
@@ -31,6 +31,7 @@
 
 #include <cmath>
 #include <limits>
+#include <vector>
 
 #include "base/algorithm.hpp"
 #include "base/logger.hpp"
@@ -38,16 +39,16 @@
 namespace prot {
 
 // if we need to increase i, return true, otherwise, return false
-bool increaseIJ(size_t i, size_t j, double deviation, 
-                double tolerance, const std::vector<double> &ms_masses, 
+bool increaseIJ(size_t i, size_t j, double deviation,
+                double tolerance, const std::vector<double> &ms_masses,
                 const std::vector<double> &theo_masses) {
   // we assume that each real peak is matched to at most one theoretical
   // peak, so we do not check i and j+1
   if (deviation <= 0) {
     return true;
   }
-  // severl real peak can be matched to the same theoretical peak 
-  if (i >= ms_masses.size() - 1 ) {
+  // severl real peak can be matched to the same theoretical peak
+  if (i >= ms_masses.size() - 1) {
     return false;
   }
 
@@ -59,18 +60,18 @@ bool increaseIJ(size_t i, size_t j, double deviation,
     j_is_closer = std::abs(next_pos - theo_masses[j]) < std::abs(next_pos - theo_masses[j + 1]);
   }
 
-  if (std::abs(next_pos - theo_masses[j]) <= tolerance  && j_is_closer) { 
+  if (std::abs(next_pos - theo_masses[j]) <= tolerance  && j_is_closer) {
     return true;
   } else {
     return false;
   }
 }
 
-// compute deviation for each peak 
-std::vector<double> compMsMassPpos(const std::vector<double> &ms_masses, 
-                                   const std::vector<double> &theo_masses, 
+// compute deviation for each peak
+std::vector<double> compMsMassPpos(const std::vector<double> &ms_masses,
+                                   const std::vector<double> &theo_masses,
                                    double ppo) {
-  // extendMsThree do not have 0 and precursor mass 
+  // extendMsThree do not have 0 and precursor mass
   std::vector<double> min_distances;
   for (size_t i = 0; i < ms_masses.size(); i++) {
     min_distances.push_back(std::numeric_limits<double>::infinity());
@@ -93,24 +94,22 @@ std::vector<double> compMsMassPpos(const std::vector<double> &ms_masses,
   std::vector<double> result_ppos;
   for (i = 0; i < ms_masses.size(); i++) {
     if (ms_masses[i] > 0) {
-      result_ppos.push_back (min_distances[i]/ ms_masses[i]); 
-    }
-    else {
+      result_ppos.push_back(min_distances[i] / ms_masses[i]);
+    } else {
       result_ppos.push_back(std::numeric_limits<double>::infinity());
     }
   }
   return result_ppos;
 }
 
-std::vector<double> compTheoMassPpos(const std::vector<double> &ms_masses, 
+std::vector<double> compTheoMassPpos(const std::vector<double> &ms_masses,
                                      const std::vector<double> &theo_masses,
                                      double ppo) {
-
   std::vector<double> min_distances;
   for (size_t p = 0; p < theo_masses.size(); p++) {
     min_distances.push_back(std::numeric_limits<double>::infinity());
   }
-  // extendMsThree do not have 0 and precursor mass 
+  // extendMsThree do not have 0 and precursor mass
   size_t i = 0;
   size_t j = 0;
   while (i < ms_masses.size() && j < theo_masses.size()) {
@@ -129,20 +128,19 @@ std::vector<double> compTheoMassPpos(const std::vector<double> &ms_masses,
   std::vector<double> result_ppos;
   for (i = 0; i < theo_masses.size(); i++) {
     if (theo_masses[i] > 0) {
-      result_ppos.push_back (min_distances[i]/ theo_masses[i]); 
-    }
-    else {
+      result_ppos.push_back(min_distances[i] / theo_masses[i]);
+    } else {
       result_ppos.push_back(std::numeric_limits<double>::infinity());
     }
   }
   return result_ppos;
 }
 
-// compute the number of matched theoretical masses (fragment ions) 
-double compNumMatchedTheoMasses (const std::vector<double> &ms_masses, 
-                                 const std::vector<double> &theo_masses, 
-                                 double ppo) {
-  std::vector<double> theo_mass_ppos 
+// compute the number of matched theoretical masses (fragment ions)
+double compNumMatchedTheoMasses(const std::vector<double> &ms_masses,
+                                const std::vector<double> &theo_masses,
+                                double ppo) {
+  std::vector<double> theo_mass_ppos
       = compTheoMassPpos(ms_masses, theo_masses, ppo);
   double score = 0;
   for (size_t i = 0; i < theo_mass_ppos.size(); i++) {
@@ -153,14 +151,14 @@ double compNumMatchedTheoMasses (const std::vector<double> &ms_masses,
   return score;
 }
 
-// compute the position of the last residue of a proteoform based 
-// on its n term shift 
-int getFirstResPos(double n_term_shift, const std::vector<double> &prm_masses){
+// compute the position of the last residue of a proteoform based
+// on its n term shift
+int getFirstResPos(double n_term_shift, const std::vector<double> &prm_masses) {
   double trunc_mass = - n_term_shift;
   int best_pos = -1;
   double best_shift = std::numeric_limits<double>::infinity();
-  for(size_t i = 0; i < prm_masses.size();i++){
-    if(std::abs(prm_masses[i] - trunc_mass) < best_shift){
+  for (size_t i = 0; i < prm_masses.size(); i++) {
+    if (std::abs(prm_masses[i] - trunc_mass) < best_shift) {
       best_pos = i;
       best_shift = std::abs(prm_masses[i] - trunc_mass);
     }
@@ -168,24 +166,24 @@ int getFirstResPos(double n_term_shift, const std::vector<double> &prm_masses){
   return best_pos;
 }
 
-// compute the position of the last residue of a proteoform based on 
-// its c term shift 
-int getLastResPos(double c_term_shift, const std::vector<double> &prm_masses){
+// compute the position of the last residue of a proteoform based on
+// its c term shift
+int getLastResPos(double c_term_shift, const std::vector<double> &prm_masses) {
   double trunc_mass = -c_term_shift;
   int best_pos = -1;
   double best_shift = std::numeric_limits<double>::infinity();
   double residue_mass_sum = prm_masses[prm_masses.size()-1];
-  for(size_t i=0;i<prm_masses.size();i++){
-    if (std::abs(residue_mass_sum-prm_masses[i]-trunc_mass)<best_shift){
-      best_pos=i;
+  for (size_t i = 0; i < prm_masses.size(); i++) {
+    if (std::abs(residue_mass_sum-prm_masses[i]-trunc_mass) < best_shift) {
+      best_pos = i;
       best_shift = std::abs(residue_mass_sum-prm_masses[i]-trunc_mass);
     }
   }
-  if(best_pos < 0){
+  if (best_pos < 0) {
     LOG_ERROR("get last residue position error! ");
     throw "get last residue position error!";
   }
   return best_pos - 1;
 }
 
-}
+}  // namespace prot
