da7f326093bae1b6ee7c91b5c7d835cc07de211f
Qiang Kou <qkou@umail.iu.edu>
2017-10-06 16:58:47 -0400

    [Windows] use SearchPath to get the correct execution directory. close #338 (#340)

--- releases/zip-win-topfd-gui.bat
+++ releases/zip-win-topfd-gui.bat
@@ -6,7 +6,7 @@ xcopy /S ..\toppic_resources topfd-win-gui-%1\toppic_resources
 
 copy C:\msys64\mingw64\bin\libgcc_s_seh-1.dll topfd-win-gui-%1
 copy C:\msys64\mingw64\bin\libwinpthread-1.dll topfd-win-gui-%1
-copy C:\msys64\mingw64\bin\libstdc++-6.dll topfd-win-gui-%1
+copy "C:\msys64\mingw64\bin\libstdc++-6.dll" topfd-win-gui-%1
 copy C:\msys64\mingw64\bin\zlib1.dll topfd-win-gui-%1
 
 copy C:\msys64\mingw64\bin\libboost_filesystem-mt.dll topfd-win-gui-%1
--- src/base/file_util.cpp
+++ src/base/file_util.cpp
@@ -34,194 +34,184 @@
 #include <string>
 #include <algorithm>
 
+#if defined (_WIN32) || defined (_WIN64) || defined (__MINGW32__) || defined (__MINGW64__)
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
 #include "base/logger.hpp"
 #include "base/file_util.hpp"
-
-#ifdef PROT_LINUX
-
-#include <unistd.h>
-
-#endif
 
 namespace fs = boost::filesystem;
 
 namespace prot {
 
-#ifdef PROT_LINUX
-  //linux
-  std::string FileUtil::getFileSeparator() {
-    return "/";
-  }
-
-  std::string FileUtil::getExecutiveDir(const std::string &argv_0){
-    int buffer_size = 1024;
-    char* buffer = new char[buffer_size];
-    size_t len = readlink ("/proc/self/exe", buffer, buffer_size);
-    std::string file_name;
-    buffer[len] = '\0';
-    file_name = std::string (buffer);
-    delete[] buffer;
-    fs::path full_path(file_name);
-    std::string exe_dir = full_path.remove_filename().string();
-    return exe_dir;
-  }
-
+std::string FileUtil::getFileSeparator() {
+#if defined (_WIN32) || defined (_WIN64) || defined (__MINGW32__) || defined (__MINGW64__)
+  return "\\";
 #else
-  //windows
-  std::string FileUtil::getFileSeparator() {
-    return "\\";
-  }
-
-  std::string FileUtil::getExecutiveDir(const std::string &argv_0){
-    fs::path full_path;
-    full_path = fs::system_complete( fs::path(argv_0));
-
-    std::string exe_dir = full_path.remove_filename().string();
-    return exe_dir;
-  }
+  return "/";
 #endif
+}
 
-  std::string FileUtil::basename(const std::string &s){
-    size_t dot_pos = s.find_last_of(".");
-    if (dot_pos < s.length()){
-      return s.substr(0, dot_pos);
-    }
-    return s;
+std::string FileUtil::getExecutiveDir(const std::string &argv_0) {
+#if defined (_WIN32) || defined (_WIN64) || defined (__MINGW32__) || defined (__MINGW64__)
+  LPSTR lpFilePart;
+  char file_name[MAX_PATH];
+  SearchPath(NULL, argv_0.c_str(), ".exe", MAX_PATH, file_name, &lpFilePart);
+#else
+  int buffer_size = 1024;
+  char* buffer = new char[buffer_size];
+  size_t len = readlink("/proc/self/exe", buffer, buffer_size);
+  std::string file_name;
+  buffer[len] = '\0';
+  file_name = std::string(buffer);
+  delete[] buffer;
+#endif
+  fs::path full_path(file_name);
+  std::string exe_dir = full_path.remove_filename().string();
+  std::cout << "exe_dir " << exe_dir << std::endl;
+  return exe_dir;
+}
+
+std::string FileUtil::basename(const std::string &s) {
+  size_t dot_pos = s.find_last_of(".");
+  if (dot_pos < s.length()) {
+    return s.substr(0, dot_pos);
+  }
+  return s;
+}
+
+std::string FileUtil::directory(const std::string &s) {
+  fs::path path(s);
+  std::string parent_dir = path.parent_path().string();
+  return parent_dir;
+}
+
+void FileUtil::createFolder(const std::string &folder_name) {
+  boost::filesystem::path path(folder_name);
+  boost::filesystem::create_directories(path);
+}
+
+void FileUtil::copyFile(const std::string &file_name,
+                        const std::string &to_file, bool over_write) {
+  boost::filesystem::path from_path(file_name);
+  boost::filesystem::path to_path(to_file);
+  if (!boost::filesystem::exists(from_path)) {
+    LOG_ERROR("source file " << file_name << " does not exist!");
+    return;
   }
 
-  std::string FileUtil::directory(const std::string &s){
-    fs::path path(s);
-    std::string parent_dir = path.parent_path().string();
-    //std::cout << "parent dir " << parent_dir << std::endl;
-    return parent_dir;
-  }
-
-  void FileUtil::createFolder(const std::string &folder_name){
-    //std::cout<<"create folder "<<folder_name<<std::endl;
-    boost::filesystem::path path(folder_name);
-    boost::filesystem::create_directories(path);
-  }
-
-  void FileUtil::copyFile(const std::string &file_name,
-                          const std::string &to_file, bool over_write){
-    //std::cout<<"copy file: "<<file_name<<" to "<<to_file<<std::endl;
-    boost::filesystem::path from_path(file_name);
-    boost::filesystem::path to_path(to_file);
-    if(!boost::filesystem::exists(from_path)){
-      LOG_ERROR( "source file "<<file_name<<" does not exist!");
+  if (boost::filesystem::exists(to_path)) {
+    if (over_write) {
+      boost::filesystem::remove(to_path);
+    } else {
       return;
     }
-
-    if(boost::filesystem::exists(to_path)){
-      if(over_write){
-        boost::filesystem::remove(to_path);
-      }
-      else{
-        return;
-      }
-    }
-
-    boost::filesystem::copy(from_path,to_path);
   }
 
-  bool FileUtil::copyDir(boost::filesystem::path const & source, 
-                         boost::filesystem::path const & destination){
-    try{
-      if(!fs::exists(source) || !fs::is_directory(source)){
-        return false;
-      }
-      if(fs::exists(destination)){
-        return false;
-      }
+  boost::filesystem::copy(from_path, to_path);
+}
 
-      if(!fs::create_directory(destination)){
-        std::cerr << "Unable to create destination directory"
-            << destination.string() << std::endl;
-        return false;
-      }
+bool FileUtil::copyDir(boost::filesystem::path const & source,
+                       boost::filesystem::path const & destination) {
+  try {
+    if (!fs::exists(source) || !fs::is_directory(source)) {
+      return false;
     }
-    catch(fs::filesystem_error const & e){
-      std::cerr << e.what() << std::endl;
+    if (fs::exists(destination)) {
       return false;
     }
 
-    for(fs::directory_iterator file(source); file != fs::directory_iterator(); ++file ){
-      try{
-        fs::path current(file->path());
-        if(fs::is_directory(current)){
-          if(!copyDir(current, destination / current.filename())){
-            return false;
-          }
-        }
-        else{
-          fs::copy_file(current, destination / current.filename());
-        }
-      }
-      catch(fs::filesystem_error const & e){
-        std:: cerr << e.what() << std::endl;
-      }
-    }
-    return true;
-  }
-
-  void FileUtil::delDir(const std::string &path){
-    fs::path dir(path);
-
-    if(fs::exists(dir))
-      fs::remove_all(dir);
-  }
-
-  void FileUtil::delFile(const std::string &path){
-    fs::path dir(path);
-
-    if(fs::exists(dir))
-      fs::remove(dir);
-  }
-
-  void clean_prefix(const fs::path & sp, const std::string & prefix) {
-    fs::directory_iterator end_iter;
-    for(fs::directory_iterator dir_iter(absolute(sp).parent_path()) ; dir_iter != end_iter ; ++dir_iter){
-      std::string filename = dir_iter->path().string();
-      if (filename.compare(0, prefix.length(), prefix) == 0){
-        fs::remove(dir_iter->path());
-      }
+    if (!fs::create_directory(destination)) {
+      std::cerr << "Unable to create destination directory"
+          << destination.string() << std::endl;
+      return false;
     }
   }
+  catch(fs::filesystem_error const & e) {
+    std::cerr << e.what() << std::endl;
+    return false;
+  }
 
-  void FileUtil::cleanDir(const std::string &fa_path, const std::string & sp_path) {
-    fs::path fa(fa_path);
-    fs::path sp(sp_path);
-    std::string fa_base = absolute(fa).string();
-    std::string sp_base = basename(absolute(sp).string());
+  for (fs::directory_iterator file(source); file != fs::directory_iterator(); ++file) {
+    try {
+      fs::path current(file->path());
+      if (fs::is_directory(current)) {
+        if (!copyDir(current, destination / current.filename())) {
+          return false;
+        }
+      } else {
+        fs::copy_file(current, destination / current.filename());
+      }
+    }
+    catch(fs::filesystem_error const & e) {
+      std:: cerr << e.what() << std::endl;
+    }
+  }
+  return true;
+}
 
-    clean_prefix(fa, fa_base + "_");
-    delFile(absolute(sp).string() + "_index");
-    delFile(sp_base + ".ZERO_PTM");
-    clean_prefix(sp, sp_base + ".ZERO_PTM_");
-    clean_prefix(sp, sp_base + ".ZERO_FILTER_");
-    delFile(sp_base + ".ONE_PTM");
-    clean_prefix(sp, sp_base + ".ONE_PTM_");
-    delFile(sp_base + ".DIAG_FILTER");
-    clean_prefix(sp, sp_base + ".DIAG_FILTER_");
-    delFile(sp_base + ".PTM");
-    clean_prefix(sp, sp_base + ".PTM_");
-    delFile(sp_base + ".TOP");
-    delFile(sp_base + ".TOP_PRE");
-    delFile(sp_base + ".GRAPH_FILTER");
-    clean_prefix(sp, sp_base + ".GRAPH_ALIGN_");
-    clean_prefix(sp, sp_base + ".VAR1_");
-    clean_prefix(sp, sp_base + ".VAR2_");
-    delFile(sp_base + ".GRAPH_ALIGN");
-    delFile(sp_base + ".CUTOFF_RESULT_SPEC");
-    delFile(sp_base + ".CUTOFF_RESULT_FORM");
-    delFile(sp_base + ".LOCAL_RESULT");
-    clean_prefix(sp, sp_base + ".EVALUE_");
-    clean_prefix(sp, sp_base + ".SPECIES_");
-    clean_prefix(sp, sp_base + ".PROT_");
-    delFile(sp_base + ".EVALUE");
-    delFile(sp_base + ".RAW_RESULT");      
-    delFile(sp_base + ".FORMS");
-    delFile(sp_base + ".FORM_RESULT");
-    delFile(sp_base + ".FORM_FILTER_RESULT");
+void FileUtil::delDir(const std::string &path) {
+  fs::path dir(path);
+
+  if (fs::exists(dir))
+    fs::remove_all(dir);
+}
+
+void FileUtil::delFile(const std::string &path) {
+  fs::path dir(path);
+
+  if (fs::exists(dir))
+    fs::remove(dir);
+}
+
+void clean_prefix(const fs::path & sp, const std::string & prefix) {
+  fs::directory_iterator end_iter;
+  for (fs::directory_iterator dir_iter(absolute(sp).parent_path());
+       dir_iter != end_iter ; ++dir_iter) {
+    std::string filename = dir_iter->path().string();
+    if (filename.compare(0, prefix.length(), prefix) == 0) {
+      fs::remove(dir_iter->path());
+    }
   }
 }
+
+void FileUtil::cleanDir(const std::string &fa_path, const std::string & sp_path) {
+  fs::path fa(fa_path);
+  fs::path sp(sp_path);
+  std::string fa_base = absolute(fa).string();
+  std::string sp_base = basename(absolute(sp).string());
+
+  clean_prefix(fa, fa_base + "_");
+  delFile(absolute(sp).string() + "_index");
+  delFile(sp_base + ".ZERO_PTM");
+  clean_prefix(sp, sp_base + ".ZERO_PTM_");
+  clean_prefix(sp, sp_base + ".ZERO_FILTER_");
+  delFile(sp_base + ".ONE_PTM");
+  clean_prefix(sp, sp_base + ".ONE_PTM_");
+  delFile(sp_base + ".DIAG_FILTER");
+  clean_prefix(sp, sp_base + ".DIAG_FILTER_");
+  delFile(sp_base + ".PTM");
+  clean_prefix(sp, sp_base + ".PTM_");
+  delFile(sp_base + ".TOP");
+  delFile(sp_base + ".TOP_PRE");
+  delFile(sp_base + ".GRAPH_FILTER");
+  clean_prefix(sp, sp_base + ".GRAPH_ALIGN_");
+  clean_prefix(sp, sp_base + ".VAR1_");
+  clean_prefix(sp, sp_base + ".VAR2_");
+  delFile(sp_base + ".GRAPH_ALIGN");
+  delFile(sp_base + ".CUTOFF_RESULT_SPEC");
+  delFile(sp_base + ".CUTOFF_RESULT_FORM");
+  delFile(sp_base + ".LOCAL_RESULT");
+  clean_prefix(sp, sp_base + ".EVALUE_");
+  clean_prefix(sp, sp_base + ".SPECIES_");
+  clean_prefix(sp, sp_base + ".PROT_");
+  delFile(sp_base + ".EVALUE");
+  delFile(sp_base + ".RAW_RESULT");
+  delFile(sp_base + ".FORMS");
+  delFile(sp_base + ".FORM_RESULT");
+  delFile(sp_base + ".FORM_FILTER_RESULT");
+}
+}  // namespace prot
