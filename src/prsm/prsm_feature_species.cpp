4fe7ac71e62ee963fabc447d4fca3db5392ca38f
Qiang Kou <qkou@umail.iu.edu>
2017-10-19 13:47:13 -0400

    [TopFD] change msft file into a tab delimited table. close #357 (#358)

--- src/prsm/prsm_feature_species.cpp
+++ src/prsm/prsm_feature_species.cpp
@@ -32,6 +32,8 @@
 #include <algorithm>
 #include <vector>
 
+#include <boost/algorithm/string.hpp>
+
 #include "base/file_util.hpp"
 #include "base/proteoform_util.hpp"
 #include "prsm/prsm_reader.hpp"
@@ -111,19 +113,32 @@ void PrsmFeatureSpecies::process() {
   PrsmStrPtrVec prsm_ptrs = PrsmReader::readAllPrsmStrs(input_file_name);
   PrsmReaderPtr prsm_reader = std::make_shared<PrsmReader>(input_file_name);
   FastaIndexReaderPtr seq_reader = std::make_shared<FastaIndexReader>(db_file_name_);
-  MsAlignReaderPtr msreader
-      = std::make_shared<MsAlignReader>(feature_file_name_,
-                                        prsm_para_ptr_->getGroupSpecNum(),
-                                        prsm_para_ptr_->getSpParaPtr()->getActivationPtr(),
-                                        prsm_para_ptr_->getSpParaPtr()->getSkipList());
-  SpectrumSetPtr spec_set_ptr = msreader->getNextSpectrumSet(prsm_para_ptr_->getSpParaPtr())[0];
+
+  // read TopFD featuers
+  std::vector<int> feature_spec_ids;
+  std::vector<int> feature_ids;
+  std::vector<double> feature_intens;
+  std::ifstream infile(feature_file_name_);
+  std::string line;
+  while (std::getline(infile, line)) {
+    if (line[0] == '#' || line == "" || line[0] == 'I') {
+      continue;
+    }
+    std::vector<std::string> strs; 
+    boost::split(strs, line, boost::is_any_of("\t "));
+    feature_spec_ids.push_back(std::stoi(strs[0]));
+    feature_ids.push_back(std::stoi(strs[6]));
+    feature_intens.push_back(std::stod(strs[7]));
+  }
+  infile.close();
+
+  size_t k = 0;
   for (size_t i = 0; i < prsm_ptrs.size(); i++) {
     int spec_id = prsm_ptrs[i]->getSpectrumId();
-    while (spec_set_ptr->getDeconvMsPtrVec()[0]->getMsHeaderPtr()->getId() != spec_id) {
-      spec_set_ptr = msreader->getNextSpectrumSet(prsm_para_ptr_->getSpParaPtr())[0];
-    }
-    prsm_ptrs[i]->setPrecFeatureId(spec_set_ptr->getDeconvMsPtrVec()[0]->getMsHeaderPtr()->getFeatureId());
-    prsm_ptrs[i]->setPrecFeatureInte(spec_set_ptr->getDeconvMsPtrVec()[0]->getMsHeaderPtr()->getFeatureInte());
+    while (feature_spec_ids[k] != spec_id) {k++;}
+    prsm_ptrs[i]->setPrecFeatureId(feature_ids[k]);
+    prsm_ptrs[i]->setPrecFeatureInte(feature_intens[k]);
+
     PrsmPtr prsm_ptr = prsm_reader->readOnePrsm(seq_reader, fix_mod_ptr_vec_);
     if (prsm_ptr != nullptr) {
       prsm_ptrs[i]->setProteinMatchSeq(prsm_ptr->getProteoformPtr()->getProteinMatchSeq());
