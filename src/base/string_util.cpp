1ab6e4f7ba4f038c1a328845d8a1708b4d74431c
Qiang Kou <qkou@umail.iu.edu>
2017-11-28 16:14:27 -0500

    add more argument checking for file name. close #427 (#430)

--- src/base/string_util.cpp
+++ src/base/string_util.cpp
@@ -109,5 +109,10 @@ std::string StringUtil::rmComment(const std::string &ori_s, const std::string &c
   return s;
 }
 
+bool StringUtil::endsWith(const std::string &str, const std::string &suffix) {
+  return str.size() >= suffix.size() &&
+      str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
 }  // namespace prot
 
