6f720f993b9d4597b3f5e14f0809fab6e1ce4fcd
Qiang Kou <qkou@umail.iu.edu>
2017-12-04 10:03:39 -0500

    deleted: testcases/evaluefactor/test_st.sh

--- testcases/evaluefactor/test_st.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir tmp
-cd tmp
-ln -s  ../../data/st.fasta ./ 
-ln -s  ../../data/st_no_digestion.msalign ./
-cd ..
-../../bin/test_evalue_factor tmp/st.fasta tmp/st_no_digestion.msalign -c C57 -d -t FDR| tee tmp/log 
