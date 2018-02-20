0302785687ad5396f30dc1e92ccd30e2900f519e
Qiang Kou <qkou@umail.iu.edu>
2018-02-20 17:13:13 -0500

    [TopPIC/TopMG] add the parameter -k to the help message. close #516 (#518)

--- src/console/topmg_argument.cpp
+++ src/console/topmg_argument.cpp
@@ -173,7 +173,8 @@ bool Argument::parse(int argc, char* argv[]) {
         ("proteo-graph-dis,j", po::value<std::string> (&proteo_graph_dis), "<a positive number>. Gap in constructing proteoform graph. Default value: 40.")        
         ("var-ptm-in-gap,G", po::value<std::string>(&var_ptm_in_gap) , "<a positive number>. Maximum number of variable PTMs in a proteform graph gap. Default value: 5.")
         ("use-asf-diagonal,D", "Use the ASF-DIAGONAL method for protein filtering.")
-        ("var-ptm,P", po::value<std::string>(&var_ptm_num) , "<a positive number>. Maximum number of variable PTMs. Default value: 10.");
+        ("var-ptm,P", po::value<std::string>(&var_ptm_num) , "<a positive number>. Maximum number of variable PTMs. Default value: 10.")
+        ("keep-temp-files,k", "Keep temporary files.");
 
     po::options_description desc("Options");
 
@@ -190,7 +191,7 @@ bool Argument::parse(int argc, char* argv[]) {
         ("proteoform-cutoff-type,T", po::value<std::string> (&cutoff_proteoform_type), "")
         ("proteoform-cutoff-value,V", po::value<std::string> (&cutoff_proteoform_value), "")
         ("filtering-result-number,o", po::value<std::string>(&filtering_result_num), "Filtering result number. Default value: 20.")
-        ("keep-temp-files,k", "Keep temporary files.")
+        ("keep-temp-files,k", "")
         ("generating-function,g", "")
         ("full-binary-path,b", "Full binary path.")
         ("mod-file-name,i", po::value<std::string>(&residue_mod_file_name), "")
