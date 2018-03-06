ba9dac19fbf2dc49ed83d38f463062498c9833b6
Qiang Kou <qkou@umail.iu.edu>
2018-03-06 17:34:45 -0500

    [TopMG] cleanup and remove possible duplicated alignment results. (#534)

--- src/graphalign/graph_align.hpp
+++ src/graphalign/graph_align.hpp
@@ -33,40 +33,63 @@ class GraphAlign {
  public:
   GraphAlign(GraphAlignMngPtr mng_ptr, ProteoGraphPtr proteo_graph_ptr,
              SpecGraphPtr spec_graph_ptr);
+
   void process();
 
   PrsmPtr geneResult(int s, int m);
+
   PrsmPtr geneResult(int s);
+
  private:
   GraphAlignMngPtr mng_ptr_;
+
   ProteoGraphPtr proteo_graph_ptr_;
+
   MassGraphPtr pg_;
+
   int proteo_ver_num_;
+
   SpecGraphPtr spec_graph_ptr_;
+
   MassGraphPtr sg_;
+
   int spec_ver_num_;
+
   int n_unknown_shift_;
+
   DistVec spec_dist_;
+
   DistVec2D dist_vec_;
+
   ConsistentPairs cons_pairs_;
+
   GraphDpNodePtrVec2D table_;
+
   GraphResultNodePtrVec3D result_nodes_;
 
   GraphResultNodePtrVec2D nodes_2d_;
+
   DiagonalHeaderPtrVec diag_headers_; 
+
   DiagonalHeaderPtrVec2D diag_headers_2d_; 
 
   void getConsistentPairs();
+
   void addToConsistentPairs(int m, const std::vector<std::pair<int, int>> & sp_pair_ij,
                             const std::vector<std::pair<int, int>> & pg_pair_ij);
+
   void initTable();
 
   GraphDpNodePtr compBestVariableNode(int i, int j, int s, int m, int &best_edge_mod_num);
+
   GraphDpNodePtr compBestShiftNode(int i, int j, int s, int m);
+
   void updateBestShiftNode(int i, int j, int s, int m);
+
   void dp();
 
   GraphResultNodePtrVec backtrace(int s, int m);
+
   void backtrace();
 
   void getNodeDiagonals(int s, int m);
