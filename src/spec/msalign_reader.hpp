1339854f61ef4db9dcbed2b7c6fdcc7fa23d4fb3
Qiang Kou <qkou@umail.iu.edu>
2018-04-25 11:51:25 -0400

    [SPEC] set a limit on number of peaks. default limit is 500. (#567)

--- src/spec/msalign_reader.hpp
+++ src/spec/msalign_reader.hpp
@@ -32,11 +32,13 @@ namespace prot {
 class MsAlignReader {
  public:
   MsAlignReader(const std::string &file_name, int group_spec_num,
-                ActivationPtr act_ptr, const std::set<std::string> skip_list):
+                ActivationPtr act_ptr, const std::set<std::string> skip_list,
+                int peak_num_limit = 500):
       file_name_(file_name),
       group_spec_num_(group_spec_num),
       activation_ptr_(act_ptr),
-      skip_list_(skip_list) {
+      skip_list_(skip_list),
+      peak_num_limit_(peak_num_limit) {
         input_.open(file_name.c_str(), std::ios::in);
         if (!input_.is_open()) {
           LOG_ERROR("msalign file  " << file_name << " does not exist.");
@@ -54,13 +56,22 @@ class MsAlignReader {
 
  private:
   std::string file_name_;
+
   int group_spec_num_;
+
   std::ifstream input_;
+
   std::vector<std::string> spectrum_str_vec_;
+
   int current_ = 0;
+
   ActivationPtr activation_ptr_;
+
   DeconvMsPtr deconv_ms_ptr_ = nullptr;
+
   std::set<std::string> skip_list_;
+
+  int peak_num_limit_;
 
   void readNext();
 };
