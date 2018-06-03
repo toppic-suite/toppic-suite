af290425cf62e91160a4205a89a8706a0e63c30a
Qiang Kou <qkou@umail.iu.edu>
2018-06-02 22:28:19 -0400

    add quant output table (#578)

---
+++ src/console/topquant_argument.cpp
@@ -0,0 +1,197 @@
+//Copyright (c) 2014 - 2018, The Trustees of Indiana University.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include <iomanip>
+#include <string>
+#include <algorithm>
+
+#include "boost/algorithm/string.hpp"
+
+#include "base/file_util.hpp"
+#include "base/xml_dom_util.hpp"
+#include "console/topquant_argument.hpp"
+
+
+namespace prot {
+
+Argument::Argument() {
+  initArguments();
+}
+
+void Argument::initArguments() {
+  arguments_["oriDatabaseFileName"]="";
+  arguments_["databaseFileName"] = "";
+  arguments_["databaseBlockSize"] = "1000000";
+  arguments_["spectrumFileName"] = "";
+  arguments_["combinedOutputName"] = "combined";
+  arguments_["activation"] = "FILE";
+  arguments_["searchType"] = "TARGET";
+  arguments_["fixedMod"] = "";
+  arguments_["ptmNumber"] = "1";
+  arguments_["errorTolerance"] = "15";
+  arguments_["cutoffSpectralType"] = "EVALUE";
+  arguments_["cutoffSpectralValue"] = "0.01";
+  arguments_["cutoffProteoformType"] = "EVALUE";
+  arguments_["cutoffProteoformValue"] = "0.01";
+  arguments_["allowProtMod"] = "NONE,NME,NME_ACETYLATION,M_ACETYLATION";
+  arguments_["numOfTopPrsms"] = "1";
+  arguments_["maxPtmMass"] = "500";
+  arguments_["minPtmMass"] = "-500";
+  arguments_["useGf"] = "false";
+  arguments_["executiveDir"] = ".";
+  arguments_["resourceDir"] = "";
+  arguments_["keepTempFiles"] = "false";
+  arguments_["fullBinaryPath"] = "false";
+  arguments_["local_threshold"] = "0.45";
+  arguments_["groupSpectrumNumber"] = "1";
+  arguments_["filteringResultNumber"] = "20";
+  arguments_["residueModFileName"] = "";
+  arguments_["threadNumber"] = "1";
+  arguments_["useFeatureFile"] = "true";
+  arguments_["skipList"] = "";
+}
+
+void Argument::outputArguments(std::ostream &output, std::map<std::string, std::string> arguments) {}
+
+void Argument::showUsage(boost::program_options::options_description &desc) {
+  std::cout << "Usage: topquant database-file-name spectrum-file-name" << std::endl; 
+  std::cout << desc << std::endl; 
+}
+
+bool Argument::parse(int argc, char* argv[]) {
+  std::string database_file_name = "";
+  std::string argument_file_name = "";
+  std::string activation = "";
+  std::string fixed_mod = "";
+  std::string allow_mod = "";
+  std::string ptm_num = "";
+  std::string error_tole = "";
+  std::string max_ptm_mass = "";
+  std::string min_ptm_mass = "";
+  std::string cutoff_spectral_type = "";
+  std::string cutoff_spectral_value = "";
+  std::string cutoff_proteoform_type = "";
+  std::string cutoff_proteoform_value = "";
+  std::string use_table = "";
+  std::string group_num = "";
+  std::string local_threshold = "";
+  std::string filtering_result_num = "";
+  std::string residue_mod_file_name = "";
+  std::string thread_number = "";
+  std::string skip_list = "";
+  std::string combined_output_name = "";
+
+  /** Define and parse the program options*/
+  try {
+    namespace po = boost::program_options;
+    po::options_description display_desc("Options");
+
+    display_desc.add_options() 
+        ("help,h", "Print the help message.")
+        ("output,o", po::value<std::string>(&combined_output_name) , "The output file name for the combined results. Default: combined.");
+
+    po::options_description desc("Options");
+
+    desc.add_options() 
+        ("help,h", "") 
+        ("output,o", po::value<std::string>(&combined_output_name) , "")
+        ("database-file-name", po::value<std::string>(&database_file_name)->required(), "Database file name with its path.")
+        ("spectrum-file-name", po::value<std::vector<std::string> >()->multitoken()->required(), "Spectrum file name with its path.");
+
+    po::positional_options_description positional_options;
+    positional_options.add("database-file-name", 1);
+    positional_options.add("spectrum-file-name", -1);
+
+    po::variables_map vm;
+    try {
+      po::store(po::command_line_parser(argc, argv).options(desc).positional(positional_options).run(),vm); 
+      if (vm.count("help")) {
+        showUsage(display_desc);
+        return false;
+      }
+      po::notify(vm); 
+      // throws on error, so do after help in case there are any problems
+    } 
+    catch(boost::program_options::required_option& e) {
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+      showUsage(display_desc);
+      return false;
+    }
+    catch(boost::program_options::error& e) {
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+      showUsage(display_desc);
+      return false;
+    }
+    std::string argv_0 (argv[0]);
+
+    arguments_["executiveDir"] = file_util::getExecutiveDir(argv_0);
+
+    LOG_DEBUG("Executive Dir " << arguments_["executiveDir"]);
+
+    arguments_["resourceDir"] = arguments_["executiveDir"] + file_util::getFileSeparator() + file_util::getResourceDirName();
+
+    arguments_["oriDatabaseFileName"] = database_file_name;
+
+    arguments_["databaseFileName"] = arguments_["oriDatabaseFileName"] + "_target";
+
+    if (vm.count("spectrum-file-name")) {
+      spec_file_list_ = vm["spectrum-file-name"].as<std::vector<std::string> >(); 
+    }
+
+    if (vm.count("output")) {
+      arguments_["combinedOutputName"] = combined_output_name; 
+    }
+  }
+  catch(std::exception&e ) {
+    std::cerr << "Unhandled Exception in parsing command line" << e.what() << ", application will now exit" << std::endl;
+    return false;
+  }
+
+  return validateArguments();
+}
+
+bool Argument::validateArguments() {
+  if (!boost::filesystem::exists(arguments_["resourceDir"])) {
+    boost::filesystem::path p(arguments_["executiveDir"]);
+    arguments_["resourceDir"]
+        = p.parent_path().string() + file_util::getFileSeparator() + "etc" + file_util::getFileSeparator() + file_util::getResourceDirName(); 
+  }
+
+  if (!boost::filesystem::exists(arguments_["oriDatabaseFileName"])) {
+    LOG_ERROR("Database file " << arguments_["databaseFileName"] << " does not exist!");
+    return false;
+  }
+
+  if (!string_util::endsWith(arguments_["oriDatabaseFileName"], ".fasta") &&
+      !string_util::endsWith(arguments_["oriDatabaseFileName"], ".fa")) {
+    LOG_ERROR("Database file " << arguments_["oriDatabaseFileName"] << " is not a fasta file!");
+    return false;
+  }
+
+  if (arguments_["oriDatabaseFileName"].length() > 200) {
+    LOG_ERROR("Database file " << arguments_["oriDatabaseFileName"] << " path is too long!");
+    return false;
+  }
+
+  for (size_t k = 0; k < spec_file_list_.size(); k++) {
+    if (!boost::filesystem::exists(spec_file_list_[k])) {
+      LOG_ERROR(spec_file_list_[k] << " does not exist!");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+}  // namespace prot
