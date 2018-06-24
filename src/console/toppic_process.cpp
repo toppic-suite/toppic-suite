772e4af2695a1b9174dc5964e86559336f41905f
Qiang Kou <qkou@umail.iu.edu>
2018-06-24 15:35:05 -0400

    Update the error message for missing TopFD feature file. (#601)

--- src/console/toppic_process.cpp
+++ src/console/toppic_process.cpp
@@ -91,7 +91,7 @@ int TopPIC_identify(std::map<std::string, std::string> & arguments) {
 
     if (arguments["useFeatureFile"] == "true") {
       if (!boost::filesystem::exists(feature_file_name)) {
-        LOG_ERROR("TopFD feature file does not exist!. Please use -x option.");
+        LOG_ERROR("TopFD feature file does not exist!. Please use -x option in command line or select 'Missing MS1 feature file in GUI'.");
         exit(EXIT_FAILURE);
       }
     }
