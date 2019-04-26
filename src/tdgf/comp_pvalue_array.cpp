e66db19ecc8e02aee704a10e868f0709d1949fce
Xiaowen <liuxiaowencs@gmail.com>
2019-04-26 13:52:03 -0400

    Fixed issue #696.

--- src/tdgf/comp_pvalue_array.cpp
+++ src/tdgf/comp_pvalue_array.cpp
@@ -19,6 +19,7 @@
 
 #include "common/util/logger.hpp"
 #include "common/base/base_data.hpp"
+#include "common/base/mass_constant.hpp"
 #include "spec/deconv_ms_util.hpp"
 #include "spec/prm_ms_factory.hpp"
 #include "tdgf/comp_pvalue_array.hpp"
@@ -47,14 +48,26 @@ void CompPValueArray::compMultiExtremeValues(const PrmMsPtrVec &ms_six_ptr_vec,
   PrmPeakPtrVec2D prm_ptr_2d;
   for (size_t i = 0; i < ms_six_ptr_vec.size(); i++) {
     PrmPeakPtrVec prm_peak_ptrs = ms_six_ptr_vec[i]->getPeakPtrVec();
-    prm_ptr_2d.push_back(prm_peak_ptrs);
+    if (prm_peak_ptrs.size() > 0) {
+      prm_ptr_2d.push_back(prm_peak_ptrs);
+    }
   }
+  double prob_prec_mass;
+  // use proteoform mass when the strict mode is used, otherwise, the precursor
+  // mass 
+  if (strict) {
+    prob_prec_mass = prsm_ptrs[0]->getProteoformPtr()->getMass();
+  }
+  else {
+    prob_prec_mass = ms_six_ptr_vec[0]->getMsHeaderPtr()->getPrecMonoMass();
+  }
+  PeakTolerancePtr tole_ptr = mng_ptr_->prsm_para_ptr_->getSpParaPtr()->getPeakTolerancePtr();
   std::vector<double> prot_probs;
   CompProbValue::compProbArray(comp_prob_ptr_, prot_n_term_residue_ptrs_,
-                               prm_ptr_2d, prsm_ptrs, strict, prot_probs);
+                               prm_ptr_2d, prsm_ptrs, strict, prob_prec_mass, tole_ptr, prot_probs);
   std::vector<double> pep_probs;
   CompProbValue::compProbArray(comp_prob_ptr_, pep_n_term_residue_ptrs_,
-                               prm_ptr_2d, prsm_ptrs, strict, pep_probs);
+                               prm_ptr_2d, prsm_ptrs, strict, prob_prec_mass, tole_ptr, pep_probs);
   // LOG_DEBUG("probability computation complete");
   double tolerance = ms_six_ptr_vec[0]->getMsHeaderPtr()->getErrorTolerance(ppo);
   for (size_t i = 0; i < prsm_ptrs.size(); i++) {
@@ -70,9 +83,9 @@ void CompPValueArray::compMultiExtremeValues(const PrmMsPtrVec &ms_six_ptr_vec,
       // prec mass.
       if (std::abs(prsm_ptrs[i]->getOriPrecMass() - prsm_ptrs[i]->getAdjustedPrecMass()) > tolerance) {
         if (prsm_ptrs[i]->getOriPrecMass() < prsm_ptrs[i]->getAdjustedPrecMass()) {
-          prec_mass += 1;
+          prec_mass += mass_constant::getIsotopeMass();
         } else {
-          prec_mass -= 1;
+          prec_mass -= mass_constant::getIsotopeMass();
         }
       }
     }
