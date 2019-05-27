b07475176ba7b3dff3435e94b12a8aeaa48fd396
Xiaowen <liuxiaowencs@gmail.com>
2019-05-27 11:47:21 -0400

    Added frac_feature

---
+++ src/deconv/feature/frac_feature.hpp
@@ -0,0 +1,96 @@
+//Copyright (c) 2014 - 2018, The Trustees of Indiana University.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+
+#ifndef TOPPIC_DECONV_FEATURE_FRAC_FEATURE_HPP_
+#define TOPPIC_DECONV_FEATURE_FRAC_FEATURE_HPP_
+
+#include <memory>
+#include <vector>
+
+namespace toppic {
+
+class FracFeature;
+typedef std::shared_ptr<FracFeature> FracFeaturePtr;
+typedef std::vector<FracFeaturePtr> FracFeaturePtrVec;
+
+class FracFeature {
+ public:
+  FracFeature() {}
+
+  /*
+  Feature(int id, int fraction_id, double mono_mass, double inte,
+          int scan_begin, int scan_end);
+          */
+
+  FracFeature(int id, int fraction_id, double mono_mass, double inte,
+              double retent_begin, double retent_end,
+              int scan_begin, int scan_end,
+              int min_charge, int max_charge);
+
+  FracFeature(std::string line);
+
+  static void writeFeatures(const std::string &output_file_name,
+                            const FracFeaturePtrVec &features);
+
+  int getId() {return id_;}
+
+  int getFractionId() {return fraction_id_;}
+
+  double getMonoMass() {return mono_mass_;}
+
+  double getIntensity() {return intensity_;}
+
+  double getRetentBegin() {return retent_begin_;}
+
+  double getRetentEnd() {return retent_end_;}
+
+  double getRetentMiddle() {return (retent_begin_ + retent_end_)/2;}
+
+  int getScanBegin() {return scan_begin_;}
+
+  int getScanEnd() {return scan_end_;}
+
+  int getMinCharge() {return min_charge_;}
+
+  int getMaxCharge() {return max_charge_;}
+
+  static bool cmpMassInc(const FracFeaturePtr &a, const FracFeaturePtr &b) { 
+    return a->getMonoMass() < b->getMonoMass();
+  }
+
+  static bool cmpInteDec(const FracFeaturePtr &a, const FracFeaturePtr &b) { 
+    return a->getIntensity() > b->getIntensity();
+  }
+
+  static bool cmpRetentInc(const FracFeaturePtr &a, const FracFeaturePtr &b) { 
+    return a->getRetentMiddle() < b->getRetentMiddle();
+  }
+
+ protected:
+  int id_;
+  int fraction_id_;
+  double mono_mass_;
+  double intensity_;
+  double retent_begin_;
+  double retent_end_;
+  int scan_begin_;
+  int scan_end_;
+  int min_charge_;
+  int max_charge_;
+};
+
+
+}
+#endif
