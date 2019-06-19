520dd84373f26d0cf526b1a2b30a3675f177d631
Xiaowen Liu <xwliu@iupui.edu>
2019-06-18 23:05:48 -0400

    Working on issue702

--- src/feature/peak_cluster.hpp
+++ src/feature/peak_cluster.hpp
@@ -21,6 +21,7 @@
 
 #include "deconv/env/envelope.hpp"
 #include "deconv/env/real_env.hpp"
+#include "deconv/env/match_env.hpp"
 
 #include "feature/frac_feature.hpp"
 
@@ -28,19 +29,39 @@ namespace toppic {
 
 class PeakCluster:public FracFeature {
  public:
+  PeakCluster(MatchEnvPtr match_env);
+
+  void addEnvelopes(int min_charge, int max_charge, 
+                    int min_ms1_id, int max_ms1_id, 
+                    int min_scan_num, int max_scan_num,
+                    RealEnvPtrVec envs);
+
+  void clearScores();
+
+  void updateScore(bool check_pvalue);
 
  private:
+  // promex feature variables
+  int min_ms1_id_;
+  int max_ms1_id_;
+  double rep_mass_;
+  int rep_charge_;
+  int rep_scan_;
+  double rep_mz_;
+  double score_ = 0.0;
+
   double apex_time_;
   int apex_scan_;
   double apex_intensity_;
   double boundary_intensity_;
 
-  RealEnvPtrVec2D envelopes_;  
+  RealEnvPtrVec2D real_envs_;  
   EnvelopePtr theo_env_;
+
+  std::vector<double> rep_summed_peaks_;
 
   // one for even charge, one for odd charge
   std::vector<int> best_charges_;
-  std::vector<double> rep_summed_envelopes_;
   std::vector<double> env_dist_scores_;
   std::vector<double> env_corr_scores_;
   std::vector<double> env_inte_scores_;
@@ -50,6 +71,11 @@ class PeakCluster:public FracFeature {
   std::vector<double> best_dist_scores_;
   std::vector<double> best_inte_scores_;
   std::vector<double> xic_corr_between_best_charges_;
+
+  // do not know the meaning
+  int flag_;
+  bool init_score_;
+
 };
 
 }
