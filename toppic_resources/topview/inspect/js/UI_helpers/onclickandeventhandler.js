b6b918cce0009187422b029d1ed3b90febe3993f
skankara <skankara@iupui.edu>
2020-01-28 11:47:10 -0500

    Readding removed code

--- toppic_resources/topview/inspect/js/UI_helpers/onclickandeventhandler.js
+++ toppic_resources/topview/inspect/js/UI_helpers/onclickandeventhandler.js
@@ -114,3 +114,20 @@ showNonMatchedPeaks = function()
 	}
 	$('div.dataTables_scrollBody').height(400);
 }
+function showIonPeaks(ids) {
+	console.log("ids : ", ids);
+	  var elems = document.getElementsByClassName('matched_peak');
+	  for(var i = 0; elems.length > i; i++) {
+	    elems[i].style.display = 'none';
+	  }
+	  elems = document.getElementsByClassName('unmatched_peak');
+	  for(var i = 0; elems.length > i; i++) {
+	    elems[i].style.display = 'none';
+	  }
+
+	 elems = document.getElementsByName(ids);
+	    for(var j = 0; elems.length > j; j++) {
+	      elems[j].style.display  =  "";
+	      elems[j].style.background  =  "#BEECFF";
+	    }
+}
