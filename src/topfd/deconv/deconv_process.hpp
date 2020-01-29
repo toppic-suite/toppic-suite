38619f9a6dbc3265891c2ff8cc6d8158ba900714
inkwchoi <inkwchoi@iu.edu>
2020-01-29 13:18:12 -0500

    fixed msg overlapping & fixed incorrect ms data

--- src/topfd/deconv/deconv_process.hpp
+++ src/topfd/deconv/deconv_process.hpp
@@ -1,4 +1,4 @@
-//Copyright (c) 2014 - 2019, The Trustees of Indiana University.
+//Copyright (c) 2014 - 2020, The Trustees of Indiana University.
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
@@ -56,9 +56,6 @@ class DeconvProcess {
   void deconvMsTwo(RawMsPtr ms_ptr, DeconvOneSpPtr deconv_ptr, 
                    MsAlignWriterPtr ms2_writer_ptr); 
   std::string updateMsg(MsHeaderPtr header_ptr, int scan, int total_scan_num);
-  
-  std::vector<PeakPtrVec> peakCal(RawMsGroupReaderPtr reader_ptr_, MatchEnvPtrVec prev_envs);
-
  private:
 
   EnvParaPtr env_para_ptr_;
