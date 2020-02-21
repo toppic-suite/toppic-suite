875ac58c81dcb25c2970ede5efcd0e7e13d4efee
inkwchoi <inkwchoi@iu.edu>
2020-02-21 14:07:14 -0500

    added code for deleting database temp files

--- src/console/topdiff_process.cpp
+++ src/console/topdiff_process.cpp
@@ -32,9 +32,6 @@ int topDiffProcess(std::map<std::string, std::string> &arguments,
   std::string db_file_name = ori_db_file_name + "_target";
   fasta_util::dbSimplePreprocess(ori_db_file_name, db_file_name);
 
-
-  std::cout << ori_db_file_name << " " << db_file_name << std::endl;
-
   std::string base_path = file_util::absoluteDir(input_file_list[0]);
   std::string output_file_name = base_path + file_util::getFileSeparator() 
       + arguments["mergedOutputFileName"];
@@ -54,6 +51,15 @@ int topDiffProcess(std::map<std::string, std::string> &arguments,
                                              error_tole);
   merge_ptr->process();
   merge_ptr = nullptr;
+  
+  std::cout << "Deleting temporary files - started." << std::endl;
+
+  std::string fa_base = file_util::absoluteName(ori_db_file_name);
+  std::replace(fa_base.begin(), fa_base.end(), '\\', '/');
+  file_util::cleanPrefix(ori_db_file_name, fa_base + "_");
+    
+  std::cout << "Deleting temporary files - finished." << std::endl; 
+
   std::cout << "TopDiff - finished." << std::endl;
   return 0;
 }
