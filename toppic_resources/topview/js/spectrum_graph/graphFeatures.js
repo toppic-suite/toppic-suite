febcc96d69f28b4c518766adf648b956ba1c8512
skankara <skankara@iupui.edu>
2020-02-25 14:34:46 -0500

    Fixing #797

--- toppic_resources/topview/js/spectrum_graph/graphFeatures.js
+++ toppic_resources/topview/js/spectrum_graph/graphFeatures.js
@@ -7,5 +7,12 @@ class GraphFeatures{
         this.bgMinMz = 0;
         this.bgMaxMz = 0;
         this.bgColor = "orange";
+        this.sequenceData = [];
+        this.svgWidth = 910;
+        this.svgHeight = 220;
+        this.padding = {left:70, right:20, head:10, bottom:50};
+        this.adjustableHeightVal = 30;
+        this.specWidth = this.svgWidth - this.padding.left - this.padding.right;
+        this.specHeight = this.svgHeight - this.padding.head - this.padding.bottom;
     }
 }
