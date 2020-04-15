5d7471a6305e27806e682c45d4a1d5a7b9783c67
inkwchoi <inkwchoi@iu.edu>
2020-04-15 09:06:55 -0400

    Updated the version number to 1.3.3

--- src/common/util/version.hpp
+++ src/common/util/version.hpp
@@ -21,7 +21,7 @@ namespace toppic {
 
 class Version {
  public:
-  static std::string getVersion() {return "1.3.2";}
+  static std::string getVersion() {return "1.3.3";}
 };
 
 }  // namespace toppic
