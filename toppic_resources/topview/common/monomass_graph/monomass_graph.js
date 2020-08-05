c0a7189af2170f1dd734d1bbf132eb64018663c9
jiangtia <jiangtia@iu.edu>
2020-08-05 13:35:35 -0400

    update monomass_graph library

--- toppic_resources/topview/common/monomass_graph/monomass_graph.js
+++ toppic_resources/topview/common/monomass_graph/monomass_graph.js
@@ -1,61 +1,173 @@
 class MonomassGraph {
-    prsm_data;
-    nav_id;
-    div_id;
+    // prsm_data;
+    navID;
+    divID;
+    specIDList;
+    scanIDList;
+    peakList;
 
-    constructor (prsm_data, nav_id = "monoMass_nav", div_id = "monomass_div") {
+    constructor (prsm_data, navID = "monoMass_nav", divID = "monomass_div") {
         this.prsm_data = prsm_data;
-        this.nav_id = nav_id;
-        this.div_id = div_id;
+        this.navID = navID;
+        this.divID = divID;
+        this.specIDList = prsm_data.prsm.ms.ms_header.ids.split(" ");
+        this.scanIDList = prsm_data.prsm.ms.ms_header.scans.split(" ");
+        this.peakList = prsm_data.prsm.ms.peaks.peak;
     }
 
     drawGraph() {
-        let MultiScanObj = new MultiScan();
-        let ms2_ids = this.prsm_data.prsm.ms.ms_header.ids;
-        let ms2_id_list = ms2_ids.split(" ");
-        // Get scan Ids of MS2 Spectrum
-        let scanIdList = this.prsm_data.prsm.ms.ms_header.scans.split(" ");
-        scanIdList = MultiScanObj.getUniqueScanIdList(scanIdList);
-        let ms2_uniqueList = MultiScanObj.getUniqueScanIdList(ms2_id_list);
-        
-        // Add tabs of scan Ids for Mono Mass Spectrum graph
-        MultiScanObj.createMonoMassNavEements(scanIdList,this.nav_id);
+        let specIDList = this.getUniqueList(this.specIDList);
+        let scanIDList = this.getUniqueList(this.scanIDList);
+
+        this.createMonoMassNavElements(scanIDList, this.navID);
+
         let dataWithScanIdList = [];
-        ms2_uniqueList.forEach((element,i) => {
-            let tempObj = {"specId":element, "scanId":scanIdList[i], value:[]};
+
+        specIDList.forEach((element,i) => {
+            let tempObj = {"specId":element, "scanId":scanIDList[i], value:[]};
             dataWithScanIdList.push(tempObj);
         });
 
-        console.log("dataWithScanIdList:", dataWithScanIdList);
-
-        let len = ms2_uniqueList.length;
-        this.prsm_data.prsm.ms.peaks.peak.forEach(function(element,i){
+        this.peakList.forEach(function(element){
             let ion = "";
             if(element.hasOwnProperty('matched_ions_num'))
             {   
                 ion = element.matched_ions.matched_ion.ion_type + element.matched_ions.matched_ion.ion_display_position;
             }
             let tempObj = {"mz":parseFloat(element.monoisotopic_mass),"intensity":parseFloat(element.intensity),"ion":ion};
-            for(let i=0;i<len;i++)
+            for(let i=0; i < specIDList.length; i++)
             {
-                if(dataWithScanIdList[i].specId == element.spec_id)
+                if(dataWithScanIdList[i].specId === element.spec_id || dataWithScanIdList[i].specId === parseInt(element.spec_id, 10))
                 {
                     dataWithScanIdList[i].value.push(tempObj);
                 }
             }
         })
 
-        createMultipleSvgs(this.div_id, "monoMassSvg_","monoMass_svg_graph_class",dataWithScanIdList);
+        this.createMultipleSvgs(this.divID, "monoMassSvg_","monoMass_svg_graph_class",dataWithScanIdList);
 
         // add onclick events to switch nav tabs
-        let nav_id = this.nav_id;
+        let navID = this.navID;
+        let self = this;
         $(".monoMass_scanIds").click(function(){
             let value = this.getAttribute('value');
             let id = "monoMassSvg_"+value;
             // Hide all the graphs except the one clicked
-            showCorrespondingGraph(id,".monoMass_svg_graph_class");
-            $("#"+nav_id+" .active").removeClass("active");
+            self.showCorrespondingGraph(id,".monoMass_svg_graph_class");
+            $("#"+navID+" .active").removeClass("active");
             $(this).addClass("active");
         })
+    }
+
+    getUniqueList(multiScanList){
+        let uniqueList = [];
+        let uniqueIdSet = new Set();
+        multiScanList.forEach(function(element){
+            uniqueIdSet.add(element);
+        });
+        //spread operator converts array to list
+        uniqueList = [...uniqueIdSet];
+        return uniqueList ;
+    }
+
+    createMonoMassNavElements(scanIDList,id){
+        let _ul = document.getElementById(id);
+        scanIDList.forEach(function(element,i){
+            let li = document.createElement("li");
+            li.setAttribute("class","nav-item");
+            let li_id = id+"_"+element;
+            li.setAttribute("id",li_id);
+            let a = document.createElement("a");
+            a.setAttribute("class","nav-link monoMass_scanIds");
+            if(i === 0)
+            {
+                a.setAttribute("class","nav-link monoMass_scanIds active");
+            }
+            a.setAttribute("href","#!");
+            a.setAttribute("value",element);
+            a.innerHTML = "Scan "+ element;
+            li.appendChild(a);
+            _ul.appendChild(li);
+         })
+    }
+
+    generateCorrespondingGraph(current_data,id,prec_mz){
+        // Gets the svg id of the spectrum
+        let startOfId = id.split("_")[0];
+        
+        if(startOfId == "monoMassSvg")
+        {
+            let graphFeatures = new GraphFeatures();
+    
+            let sequenceObj = new Sequence(this.prsm_data);
+            let sequence = sequenceObj.getSequence();
+    
+            let ionMassShiftObj = new IonMassShift("B");
+            let ionShift = ionMassShiftObj.getIonTypeMass();
+    
+            let massShiftListObj = new MassShiftList(this.prsm_data);
+            let massShiftList = massShiftListObj.getMassShiftList();
+    
+            let cpsmObj = new CalcPrefixSuffixMassList(sequence, massShiftList);
+            let prefixMassList = cpsmObj.getPrefixMassList(ionShift);
+    
+            ionMassShiftObj.ionType = "Y";
+            ionShift = ionMassShiftObj.getIonTypeMass();
+    
+            let suffixMassList = cpsmObj.getSuffixMassList(ionShift);
+    
+            // Setting the graphFeatures object with all the features needed for the graph
+            graphFeatures.showSequence = true;
+            graphFeatures.addErrorPlot = true;
+            graphFeatures.prefixSequenceData = prefixMassList;
+            graphFeatures.suffixSequeceData = suffixMassList;
+            graphFeatures.svgHeight = graphFeatures.svgHeight + graphFeatures.adjustableHeightVal + graphFeatures.heightForErrorPlot;
+            graphFeatures.padding.head = graphFeatures.padding.head + graphFeatures.adjustableHeightVal;
+            graphFeatures.padding.bottom = graphFeatures.padding.bottom + graphFeatures.heightForErrorPlot;
+            graphFeatures.adjustableIonPosition = 10; // Random tested value for alignment
+            // Gets the data list with mass error to plot in the monomass spectrum 
+            let prsmDataUtilObj = new PrsmDataUtil(prsm_data);
+            graphFeatures.errorListData = prsmDataUtilObj.json2ErrorDataList(); 
+            // Gets the absolute max and minimum value for upper bound and lower bound of y axis to draw the error plot
+            graphFeatures.errorThreshHoldVal = this.getAbsoluteMaxValfromList(graphFeatures.errorListData);
+            // Invoking spectrum function to draw the spectrum
+            let spectrumgraph = new addSpectrum(id, current_data, null, prec_mz, current_data,graphFeatures);
+        }
+    }
+
+    createMultipleSvgs(divId,svgId,className,dataWithScanIdList){
+        let div = document.getElementById(divId); 
+        let self = this;
+        dataWithScanIdList.forEach(function(element,i){
+            let id = svgId+element.scanId;
+            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");;
+            svg.setAttribute("id",id);
+            svg.setAttribute("class",className);
+            svg.style.backgroundColor = "#F8F8F8"; 
+            if(i != 0)
+            {
+                svg.style.display = "none"; 
+            }
+            div.appendChild(svg);
+            self.generateCorrespondingGraph(element.value, id, null);
+        });
+    }
+
+    showCorrespondingGraph(id,className)
+    {
+        $(className).hide();
+        document.getElementById(id).style = "block";
+    }
+    
+    getAbsoluteMaxValfromList(errorDataList){
+        let max = 0;
+        errorDataList.forEach((element)=>{
+            let val = Math.abs(element.mass_error);
+            if(max < val) max = val; 
+        })
+        //Getting the round off fraction value
+        max = max * 100;
+        max = Math.ceil(max)/100;
+        return max;
     }
 }
