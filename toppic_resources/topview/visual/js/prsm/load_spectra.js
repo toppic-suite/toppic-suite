65f1b69644fe57607e3068e80e6db1a1045b874c
inkwon <inkwchoi@iu.edu>
2020-11-30 18:31:13 -0500

    fix #971 #978 - fixed Inspect page & save spectrum

--- toppic_resources/topview/visual/js/prsm/load_spectra.js
+++ toppic_resources/topview/visual/js/prsm/load_spectra.js
@@ -71,10 +71,6 @@ function loadMsTwo(specIdList, fileList, proteoform, divId, navId){
           let nIonType = specList[j].n_ion_type;
           let cIonType = specList[j].c_ion_type;
 
-          console.log("monoIons",monoIons)
-          console.log("nIonType",nIonType)
-          console.log("cIonType",cIonType)
-
           let monoSpGraph = new SpectrumGraph(monoSvgId,monoMasses); 
           monoSpGraph.addMonoMassSpectrumAnno(monoIons,proteoform, nIonType, cIonType);
           monoSpGraph.para.setMonoMassGraph(true);
