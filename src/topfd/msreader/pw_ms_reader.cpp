d89b47a89af9e478f5a9e9e7bdb5490c19369c68
inkwchoi <inkwchoi@iu.edu>
2021-01-13 10:07:05 -0500

    fix #1059 - topfd crash when invalid activation info

--- src/topfd/msreader/pw_ms_reader.cpp
+++ src/topfd/msreader/pw_ms_reader.cpp
@@ -19,6 +19,7 @@
 #include "common/util/logger.hpp"
 #include "common/util/str_util.hpp"
 #include "common/base/activation_base.hpp"
+#include "common/util/custom_exception.hpp"
 #include "topfd/msreader/pw_ms_reader.hpp"
 
 namespace toppic {
@@ -135,8 +136,7 @@ bool PwMsReader::readOneMs(int sp_id, PeakPtrVec &peak_list, MsHeaderPtr &header
     if (ac_name == "") {
       LOG_WARN("No activation information is available in reading the spectrum with scan " << spec_info.scanNumber);
       std::cout << "\nERROR: Unable to read the activation method from the file. Please select an activation method out of CID|HCD|ETD|UVPD and provide it as a parameter. Example: -a CID" << std::endl;
-      //exit(0);
-      exit(EXIT_FAILURE);
+      throw InvalidActivation();
     }
     LOG_DEBUG("ac name " << ac_name);
     ActivationPtr activation_ptr = ActivationBase::getActivationPtrByName(ac_name);
