e0b64871aed514222110bcb591f0f241e0a88e1f
inkwon <inkwchoi@iu.edu>
2021-01-20 08:26:03 -0500

    change version to 1.4.3

--- src/common/util/version.hpp
+++ src/common/util/version.hpp
@@ -21,7 +21,7 @@ namespace toppic {
 
 class Version {
  public:
-  static std::string getVersion() {return "1.4.2";}
+  static std::string getVersion() {return "1.4.3";}
 };
 
 }  // namespace toppic
