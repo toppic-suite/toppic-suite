3a6be8923a4568befadee5648627e1648cdd1504
inkwchoi <inkwchoi@iu.edu>
2021-02-26 14:32:50 -0500

    fix #1084 - html folder generated even when using -g

--- src/console/toppic_process.cpp
+++ src/console/toppic_process.cpp
@@ -543,12 +543,14 @@ int TopPICProgress_multi_file(std::map<std::string, std::string> & arguments,
     std::cout << "Merging msalign files started." << std::endl;
     MsAlignFracMerge::mergeFiles(spec_file_lst, full_combined_name + "_ms2.msalign", para_str);
     std::cout << "Merging msalign files finished." << std::endl;
-    std::cout << "Merging json files started." << std::endl;
-    DeconvJsonMergePtr json_merger 
-        = std::make_shared<DeconvJsonMerge>(spec_file_lst, full_combined_name);
-    json_merger->process();
-    json_merger = nullptr;
-    std::cout << "Merging json files finished." << std::endl;
+    if (arguments["geneHTMLFolder"] == "true"){
+      std::cout << "Merging json files started." << std::endl;
+      DeconvJsonMergePtr json_merger 
+          = std::make_shared<DeconvJsonMerge>(spec_file_lst, full_combined_name);
+      json_merger->process();
+      json_merger = nullptr;
+      std::cout << "Merging json files finished." << std::endl;
+    }
     std::cout << "Merging feature files started." << std::endl;
     FeatureMergePtr feature_merger 
         = std::make_shared<FeatureMerge>(spec_file_lst, full_combined_name);
