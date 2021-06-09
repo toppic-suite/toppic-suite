629b210623f6a5e5ee1df67c8ddaa838f54beca2
inkwon <inkwchoi@iu.edu>
2021-06-09 15:51:14 -0400

    added new codes for generating command for console

---
+++ src/gui/util/run_program.cpp
@@ -0,0 +1,66 @@
+//Copyright (c) 2014 - 2021, The Trustees of Indiana University.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include "common/util/logger.hpp"
+#include "gui/util/run_program.h"
+#include <vector>
+
+namespace toppic {
+
+std::string RunProgram::geneCommand(std::map<std::string, std::string> arguments_, std::string app_name) {
+  //have each argument converted to command line parameter
+  //create a string for a command at the end
+  std::string exe_path = arguments_["executiveDir"] + app_name + ".exe";
+  std::string command = exe_path;
+
+  for (std::map<std::string, std::string>::iterator it = arguments_.begin(); it != arguments_.end(); ++it) {
+    if (common_para.find(it->first) != common_para.end()) { //if one of the common parameters
+      //some parameters require extra processing
+      if (it->first == "fixedMod" && it->second == "") continue; //don't add -f
+      if (it->first == "searchType" && it->second == "TARGET") continue; // don't add -d
+      command = command + common_para[it->first] + it->second;
+    }
+    else if (app_name == "topfd") {}// this needs separate processing
+    else if (app_name == "toppic" && toppic_para.find(it->first) != toppic_para.end()) {
+      command = command + toppic_para[it->first] + it->second;
+    }
+    else if (app_name == "topindex" && topindex_para.find(it->first) != topindex_para.end()) {
+      command = command + topindex_para[it->first] + it->second;
+    }
+    else if (app_name == "topmg" && topmg_para.find(it->first) != topmg_para.end()) {
+      command = command + topmg_para[it->first] + it->second;
+    }
+    else if (app_name == "topdiff" && topdiff_para.find(it->first) != topdiff_para.end()) {
+      command = command + topdiff_para[it->first] + it->second;
+    }
+    else {//parameter is not found anywhere
+      LOG_ERROR("Parameter " << it->first << " from " << app_name << " was not found in any apps!");
+      return false;
+    }
+  }
+};
+void RunProgram::run(std::string command) {
+  char buffer[500];
+  FILE* pipe = _popen(command.c_str(), "r");
+  if (!pipe) {
+      LOG_ERROR("popen failed!");
+  }
+  while (!feof(pipe)) {
+    if (fgets(buffer, 128, pipe) != NULL) {
+        std::cout << buffer << std::endl;
+    }
+  }
+  _pclose(pipe);
+};
+}
---
+++ src/gui/util/run_program.h
@@ -0,0 +1,76 @@
+//Copyright (c) 2014 - 2021, The Trustees of Indiana University.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#ifndef GUI_RUN_PROGRAM_H
+#define GUI_RUN_PROGRAM_H
+
+#include <iostream>
+#include <string>
+#include <map>
+namespace toppic {
+class RunProgram {
+ public:
+  std::map<std::string, std::string> common_para {
+    {"activation", "-a "},
+    {"fixedMod", "-f "},
+    {"allowProtMod", "-n "},
+    {"searchType", "-d "},
+    {"threadNumber", "-u "},
+    {"proteoformErrorTolerance", "-p "},
+    {"maxPtmMass", "-M "},
+    {"minPtmMass", "-m "},
+    {"cutoffSpectralType", "-t "},
+    {"cutoffSpectralValue", "-v "},
+    {"cutoffProteoformType", "-T "},
+    {"cutoffProteoformValue", "-V "},
+    {"ptmNumber", "-s "},
+    {"useFeatureFile", "-x "},
+    {"combinedOutputName", "-c "},
+    {"keepTempFiles", "-k "},
+    {"geneHTMLFolder", "-g "}
+  };
+
+  std::map<std::string, std::string> topindex_para {
+    {"massErrorTolerance", "-e "}
+  };
+
+  std::map<std::string, std::string> toppic_para {
+    {"massErrorTolerance", "-e "},
+    {"useLookupTable", "-l "},
+    {"groupSpectrumNumber", "-r "},
+    {"residueModFileName", "-i "},
+    {"localThreshold", "-H "}
+  };
+
+  std::map<std::string, std::string> topmg_para {
+    {"massErrorTolerance", "-e "},
+    {"useAsfDiag", "-D "},
+    {"varModFileName", "-i "},
+    {"varPtmNumber", "-P "},
+    {"wholeProteinOnly", "-w "},
+    {"proteoGraphGap", "-j "},
+    {"varPtmNumInGap", "-G "}
+  };
+
+  std::map<std::string, std::string> topdiff_para {
+    {"errorTolerance", "-e "},
+    {"mergedOutputFileName", "-o "},
+    {"toolName", "-t "}
+  };
+
+  std::string geneCommand(std::map<std::string, std::string> arguments_, std::string appName);
+  void run(std::string command); 
+};
+
+}
