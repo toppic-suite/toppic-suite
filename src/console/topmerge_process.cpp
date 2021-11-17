83eca3cf446325ee5ac011a39d35cbc13fd768af
Xiaowen Liu <liuxiaowencs@gmail.com>
2021-11-16 22:11:45 -0600

    Updated the topmg_gui and topdiff_gui

--- src/console/topmerge_process.cpp
+++ src/console/topmerge_process.cpp
@@ -282,7 +282,7 @@ void TopMergeProcess(std::map<std::string, std::string> & arguments,
     Argument::outputArguments(std::cout, arguments);
     PrsmParaPtr prsm_para_ptr = std::make_shared<PrsmPara>(arguments);
 
-    int thread_num = std::stoi(arguments["threadNumber"]);
+    //int thread_num = std::stoi(arguments["threadNumber"]);
     if (arguments["combinedOutputName"] != "") {
       std::string merged_file_name = arguments["combinedOutputName"]; 
       std::string para_str = "";
