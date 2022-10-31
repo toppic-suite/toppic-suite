85c80efc493918dfe2976c6634928d6fe4d9a566
xiaowen <liuxiaowencs@gmail.com>
2022-10-31 15:55:12 -0500

    Change version to 1.5.5

--- src/common/util/version.hpp
+++ src/common/util/version.hpp
@@ -21,7 +21,7 @@ namespace toppic {
 
 class Version {
  public:
-  static std::string getVersion() {return "1.5.4";}
+  static std::string getVersion() {return "1.5.5";}
 };
 
 }  // namespace toppic
