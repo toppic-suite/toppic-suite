cc8b50c801356801fc5388566014edb97a8c6b77
xiaowen <liuxiaowencs@gmail.com>
2022-12-05 13:10:09 -0600

    Fixed #339

--- resources/topmsv/common/data_table/draw_table.ts
+++ resources/topmsv/common/data_table/draw_table.ts
@@ -358,7 +358,7 @@
               let ionPos: number = parseInt((matchedPeakPair.getIon().getId()).slice(1));
               if (ion.indexOf("X") >= 0 || ion.indexOf("Y") >= 0 || ion.indexOf("Z") >= 0) {
                 if (seqLen){
-                  td.innerHTML = (seqLen - (ionPos) + 1).toString();
+                  td.innerHTML = (seqLen - ionPos).toString();
                 }
                 else{
                   console.error("ERROR: seqLen is not provided");
