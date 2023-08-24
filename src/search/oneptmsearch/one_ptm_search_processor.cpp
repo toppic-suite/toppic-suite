378b4a130a93979d2edc43d9d505c5da7b6a98de
Xiaowen Liu <liuxiaowencs@gmail.com>
2023-08-23 23:09:51 -0400

    Fixed a bug in prsm_str_merge.cpp

--- src/search/oneptmsearch/one_ptm_search_processor.cpp
+++ src/search/oneptmsearch/one_ptm_search_processor.cpp
@@ -75,7 +75,8 @@ PrsmPtrVec OnePtmSearchProcessor::onePtmSearchOneSpec(SpectrumSetPtr spec_set_pt
     }
   }
   std::sort(prsms.begin(), prsms.end(), Prsm::cmpMatchFragmentDecMatchPeakDec);
-  if (prsms.size() > 0 && mng_ptr->n_report_ < prsms.size()) {
+  int prsm_num = prsms.size();
+  if (prsms.size() > 0 && mng_ptr->n_report_ < prsm_num) {
     prsms.erase(prsms.begin() + mng_ptr->n_report_, prsms.end());
   }
   return prsms;
