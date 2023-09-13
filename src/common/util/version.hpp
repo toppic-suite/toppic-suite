09627be323da02924649f5113f3466a514ff88dc
xiaowen <liuxiaowencs@gmail.com>
2023-09-13 13:26:23 -0500

    Fixed #388

--- src/common/util/version.hpp
+++ src/common/util/version.hpp
@@ -21,7 +21,7 @@ namespace toppic {
 
 class Version {
  public:
-  static std::string getVersion() {return "1.6.4";}
+  static std::string getVersion() {return "1.6.5";}
 };
 
 }  // namespace toppic
