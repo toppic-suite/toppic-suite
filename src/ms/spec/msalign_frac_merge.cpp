c574775a28f939fdef5191a390f6cbaf1f93baca
xiaowen <liuxiaowencs@gmail.com>
2024-07-08 15:15:37 -0500

    Completed #448 #449

--- src/ms/spec/msalign_frac_merge.cpp
+++ src/ms/spec/msalign_frac_merge.cpp
@@ -12,6 +12,7 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
+#include "common/util/logger.hpp"
 #include "common/util/file_util.hpp"
 #include "para/sp_para.hpp"
 #include "ms/spec/msalign_reader.hpp"
