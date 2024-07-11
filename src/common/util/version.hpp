f08adbe19edf8a725067023a598afb49c68e7db6
xiaowen <liuxiaowencs@gmail.com>
2024-07-11 10:55:39 -0500

    Changed version to 1.7.5

--- src/common/util/version.hpp
+++ src/common/util/version.hpp
@@ -21,7 +21,7 @@ namespace toppic {
 
 class Version {
  public:
-  static std::string getVersion() {return "1.7.4";}
+  static std::string getVersion() {return "1.7.5";}
 };
 
 }  // namespace toppic
