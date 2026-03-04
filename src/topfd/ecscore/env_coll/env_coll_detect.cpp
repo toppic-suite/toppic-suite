75410db267a1c9307f419aafb2438760fe1548eb
Xiaowen <liuxiaowencs@gmail.com>
2026-03-03 21:03:42 -0600

    Updated copyright

--- src/topfd/ecscore/env_coll/env_coll_detect.cpp
+++ src/topfd/ecscore/env_coll/env_coll_detect.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 - 2025, The Trustees of Indiana University, Tulane
+// Copyright (c) 2014 - 2026, The Trustees of Indiana University, Tulane
 // University.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
